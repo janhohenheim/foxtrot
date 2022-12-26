604b783ec68d7b1ca82409e84e3563935ed723f9
Jan Nils Ferner <contact@jnferner.com>
2022-12-26 01:29:16 +0100

    Add character controller

--- src/physics.rs
+++ src/physics.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+use crate::GameState;
 use bevy_rapier2d::prelude::*;
 
 pub struct PhysicsPlugin;
@@ -7,8 +8,15 @@ pub struct PhysicsPlugin;
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-            .add_plugin(RapierDebugRenderPlugin::default())
-            .add_startup_system(setup_graphics);
+            .insert_resource(RapierConfiguration {
+                gravity: Vec2::new(0.0, -9.81),
+                ..default()
+            })
+            .add_startup_system(setup_graphics)
+            .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(setup_physics));
+
+        #[cfg(debug_assertions)]
+        app.add_plugin(RapierDebugRenderPlugin::default());
     }
 }
 
@@ -17,4 +25,20 @@ fn setup_graphics(mut commands: Commands) {
     commands
         .spawn(Camera2dBundle::default())
         .insert(Name::new("Camera"));
+}
+
+fn setup_physics(mut commands: Commands) {
+    /* Create the ground. */
+    commands.spawn((
+        Collider::cuboid(500.0, 50.0),
+        TransformBundle::from(Transform::from_xyz(0.0, -200.0, 0.0)),
+    ));
+
+    /* Create the bouncing ball. */
+    commands.spawn((
+        RigidBody::Dynamic,
+        Collider::ball(50.0),
+        Restitution::coefficient(0.7),
+        TransformBundle::from(Transform::from_xyz(0.0, 500.0, 0.0)),
+    ));
 }
--- src/player.rs
+++ src/player.rs
@@ -2,46 +2,96 @@ use crate::actions::Actions;
 use crate::loading::TextureAssets;
 use crate::GameState;
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 pub struct PlayerPlugin;
 
 #[derive(Component)]
 pub struct Player;
 
+#[derive(Component)]
+pub struct Grounded {
+    grounded: bool,
+}
+
+impl Default for Grounded {
+    fn default() -> Self {
+        Self { grounded: false }
+    }
+}
+
 /// This plugin handles player related stuff like movement
 /// Player logic is only active during the State `GameState::Playing`
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_system_set(SystemSet::on_enter(GameState::Playing).with_system(spawn_player))
-            .add_system_set(SystemSet::on_update(GameState::Playing).with_system(move_player));
+            .add_system_set(
+                SystemSet::on_update(GameState::Playing)
+                    .with_system(move_player.label("move_player"))
+                    .with_system(add_gravity.label("add_gravity").after("move_player"))
+                    .with_system(check_ground.after("add_gravity")),
+            );
     }
 }
 
 fn spawn_player(mut commands: Commands, textures: Res<TextureAssets>) {
-    commands
-        .spawn(SpriteBundle {
+    commands.spawn((
+        RigidBody::KinematicPositionBased,
+        Collider::ball(0.5),
+        KinematicCharacterController::default(),
+        Player,
+        Grounded::default(),
+        SpriteBundle {
             texture: textures.bevy.clone(),
             transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
             ..Default::default()
-        })
-        .insert(Player);
+        },
+    ));
+}
+
+fn check_ground(mut query: Query<(&mut Grounded, &KinematicCharacterControllerOutput)>) {
+    for (mut grounded, output) in &mut query {
+        grounded.grounded = output.grounded
+    }
+}
+
+fn add_gravity(
+    time: Res<Time>,
+    mut player_query: Query<(&mut KinematicCharacterController, &Grounded)>,
+) {
+    for (mut controller, grounded) in &mut player_query {
+        if grounded.grounded {
+            continue;
+        }
+        let gravity = Vec2::new(0.0, -9.81 * 100.0 * time.delta_seconds());
+        controller.translation = Some(
+            controller
+                .translation
+                .map(|translation| translation + gravity)
+                .unwrap_or(gravity),
+        );
+    }
 }
 
 fn move_player(
     time: Res<Time>,
     actions: Res<Actions>,
-    mut player_query: Query<&mut Transform, With<Player>>,
+    mut player_query: Query<(&Grounded, &mut KinematicCharacterController), With<Player>>,
 ) {
     if actions.player_movement.is_none() {
         return;
     }
-    let speed = 150.;
-    let movement = Vec3::new(
-        actions.player_movement.unwrap().x * speed * time.delta_seconds(),
-        actions.player_movement.unwrap().y * speed * time.delta_seconds(),
-        0.,
-    );
-    for mut player_transform in &mut player_query {
-        player_transform.translation += movement;
+    let vertical_speed = 300.0;
+    let jump_speed = 1_000.0;
+    for (grounded, mut controller) in &mut player_query {
+        let movement = Vec2::new(
+            actions.player_movement.unwrap().x * vertical_speed * time.delta_seconds(),
+            if grounded.grounded {
+                actions.player_movement.unwrap().y * jump_speed * time.delta_seconds()
+            } else {
+                0.0
+            },
+        );
+        controller.translation = Some(movement);
     }
 }

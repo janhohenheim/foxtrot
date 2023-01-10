4d95170a7203e7a469cf20c7810ee01a66183896
Jan Nils Ferner <contact@jnferner.com>
2023-01-10 15:32:13 +0100

    Move values into constants

--- src/scene_editor.rs
+++ src/scene_editor.rs
@@ -115,11 +115,15 @@ fn spawn_objects(
     let game_objects = game_objects.retrieve_with(asset_server);
 
     for object in spawn_events.iter() {
-        let origin = player.translation + Vec3::new(0., 1., 0.);
+        let eye_height_offset = Vec3::new(0., 1., 0.);
+        let origin = player.translation + eye_height_offset;
         let direction = -(camera.rotation * Vec3::Z);
+
+        let offset_to_not_spawn_in_ground = Vec3::new(0., 2., 0.);
         let location =
             get_raycast_location(&origin, &direction, &rapier_context, MAX_SPAWN_DISTANCE)
-                + Vec3::new(0., 2., 0.);
+                + offset_to_not_spawn_in_ground;
+
         let bundle = game_objects.get(&object.0, Transform::from_translation(location));
         commands.spawn(bundle);
     }

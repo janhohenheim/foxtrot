0a702164fb369af28b35af04499b2df5453d9c3f
Jan Nils Ferner <contact@jnferner.com>
2023-01-19 18:59:58 +0100

    Repeat ground texture using shader
    Former-commit-id: c35e8b0cd4c74eaced8e281a13825f97b3da64ba [formerly 41d7cc2eb4c3a5accd26a3b57f94f64b351874ce]
    Former-commit-id: 686fa2659d3ae199572d463ef699dd37944e6e92

--- src/shader.rs
+++ src/shader.rs
@@ -1,8 +1,7 @@
 use crate::GameState;
 use bevy::prelude::*;
 use bevy::reflect::TypeUuid;
-use bevy::render::render_resource::{AddressMode, AsBindGroup, ShaderRef};
-use bevy::render::texture::ImageSampler;
+use bevy::render::render_resource::{AsBindGroup, ShaderRef};
 use std::path::Path;
 
 pub struct ShaderPlugin;
@@ -10,33 +9,44 @@ pub struct ShaderPlugin;
 impl Plugin for ShaderPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(MaterialPlugin::<GlowyMaterial>::default())
+            .add_plugin(MaterialPlugin::<RepeatedMaterial>::default())
             .add_system_set(SystemSet::on_enter(GameState::Loading).with_system(setup_shader))
             .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(spawn_shader))
             .add_system_set(
                 SystemSet::on_update(GameState::Playing)
                     .with_system(apply_shader)
                     .with_system(set_texture_to_repeat),
-            )
-            .add_system(set_texture_tiled);
+            );
     }
 }
 
 fn setup_shader(
     mut commands: Commands,
     mut glow_materials: ResMut<Assets<GlowyMaterial>>,
+    mut repeated_materials: ResMut<Assets<RepeatedMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
     let env_texture_path = Path::new("hdri").join("stone_alley_2.hdr");
     let env_texture = asset_server.load(env_texture_path);
-    let material = glow_materials.add(GlowyMaterial {
+    let glowy_material = glow_materials.add(GlowyMaterial {
         env_texture: Some(env_texture),
     });
-    commands.insert_resource(Materials { glowy: material });
+
+    let texture_path = Path::new("textures").join("ground_forest.png");
+    let texture = asset_server.load(texture_path);
+    let repeated_material = repeated_materials.add(RepeatedMaterial {
+        texture: Some(texture),
+    });
+    commands.insert_resource(Materials {
+        glowy: glowy_material,
+        repeated: repeated_material,
+    });
 }
 
 #[derive(Resource, Debug, Clone)]
 struct Materials {
     pub glowy: Handle<GlowyMaterial>,
+    pub repeated: Handle<RepeatedMaterial>,
 }
 
 fn spawn_shader(
@@ -81,6 +91,20 @@ impl Material for GlowyMaterial {
     }
 }
 
+#[derive(AsBindGroup, Debug, Clone, TypeUuid)]
+#[uuid = "82d336c5-fd6c-41a3-bdd4-267cd4c9be22"]
+pub struct RepeatedMaterial {
+    #[texture(0)]
+    #[sampler(1)]
+    pub texture: Option<Handle<Image>>,
+}
+
+impl Material for RepeatedMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/repeated.wgsl".into()
+    }
+}
+
 #[allow(clippy::type_complexity)]
 fn apply_shader(
     mut _commands: Commands,
@@ -98,31 +122,20 @@ fn set_texture_to_repeat(
     mut commands: Commands,
     added_name: Query<(&Name, &Children), Added<Name>>,
     material_handles: Query<&Handle<StandardMaterial>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut images: ResMut<Assets<Image>>,
+    materials: Res<Materials>,
+    mut standard_materials: ResMut<Assets<StandardMaterial>>,
 ) {
     for (name, children) in &added_name {
         if name.to_lowercase().contains("ground") {
-            let (child, material_handle) = children
+            let (child, standard_mat_handle) = children
                 .iter()
                 .filter_map(|entity| material_handles.get(*entity).ok().map(|mat| (*entity, mat)))
                 .next()
                 .unwrap();
+            let standard_material = standard_materials.get_mut(standard_mat_handle).unwrap();
+            standard_material.base_color_texture = None;
 
-            let mut material = materials.get(material_handle).unwrap().clone();
-            let image_handle = material.base_color_texture.as_ref().unwrap();
-            let mut image = images.get_mut(image_handle).unwrap().clone();
-            let mut sampler = match image.sampler_descriptor {
-                ImageSampler::Descriptor(ref mut descriptor) => descriptor,
-                _ => panic!(),
-            };
-
-            sampler.address_mode_u = AddressMode::Repeat;
-            sampler.address_mode_v = AddressMode::Repeat;
-            sampler.address_mode_w = AddressMode::Repeat;
-
-            material.base_color_texture = Some(images.add(image));
-            commands.entity(child).insert(materials.add(material));
+            commands.entity(child).insert(materials.repeated.clone());
         }
     }
 }

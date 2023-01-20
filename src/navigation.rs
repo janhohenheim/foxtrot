764eef5406da1a033d5562a9878d3aa5bc2af9c5
Jan Nils Ferner <contact@jnferner.com>
2023-01-20 21:17:31 +0100

    Compute missing global transform
    Former-commit-id: ff27e96ca6e857d332fed3fe2845f750526d9ad5 [formerly f622eb63c6f8b044d777b46560060450b4fdfd88]
    Former-commit-id: f754e3f51f789102d52fcc4a5d7f31372b62d6be

--- src/navigation.rs
+++ src/navigation.rs
@@ -1,39 +1,17 @@
 use crate::player::{CharacterVelocity, Player};
-use crate::spawning::objects::{npc, player};
 use crate::GameState;
 use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
 use bevy_pathmesh::PathMesh;
 use bevy_pathmesh::PathmeshPlugin;
 use bevy_prototype_debug_lines::DebugLines;
-use oxidized_navigation::{
-    query::{find_path, perform_string_pulling_on_path},
-    NavMesh, NavMeshSettings, OxidizedNavigationPlugin,
-};
 use serde::{Deserialize, Serialize};
-use std::iter;
 
 pub struct NavigationPlugin;
 
 impl Plugin for NavigationPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(OxidizedNavigationPlugin)
-            .add_plugin(PathmeshPlugin)
-            .insert_resource(NavMeshSettings {
-                cell_width: 0.4,
-                cell_height: 0.1,
-                tile_width: 200,
-                world_half_extents: 230.0,
-                world_bottom_bound: -10.0,
-                max_traversable_slope_radians: (40.0_f32 - 0.1).to_radians(),
-                walkable_height: 20,
-                walkable_radius: 2,
-                step_height: 3,
-                min_region_area: 100,
-                merge_region_area: 500,
-                max_contour_simplification_error: 0.5,
-                max_edge_length: 50,
-            })
+        app.add_plugin(PathmeshPlugin)
             .add_system_set(SystemSet::on_update(GameState::Playing).with_system(query_mesh));
     }
 }
@@ -52,10 +30,10 @@ fn query_mesh(
     with_player: Query<&GlobalTransform, (With<Player>, Without<Follower>)>,
     mut lines: ResMut<DebugLines>,
     path_meshes: Res<Assets<PathMesh>>,
-    nav_meshes: Query<(&GlobalTransform, &Handle<PathMesh>), (Without<Follower>, Without<Player>)>,
+    nav_meshes: Query<&Handle<PathMesh>>,
 ) {
     let dt = time.delta_seconds();
-    for (mesh_transform, path_mesh_handle) in nav_meshes.iter() {
+    for path_mesh_handle in nav_meshes.iter() {
         for (follower_transform, mut character_velocity) in &mut with_follower {
             for player_transform in &with_player {
                 let path_mesh = path_meshes.get(path_mesh_handle).unwrap();
@@ -63,17 +41,7 @@ fn query_mesh(
                     follower_transform.translation().xz(),
                     player_transform.translation().xz(),
                 );
-                let transform = mesh_transform.compute_transform();
-                let inverse_transform = Transform {
-                    translation: -transform.translation,
-                    rotation: transform.rotation.inverse(),
-                    scale: 1. / transform.scale,
-                };
-                info!(
-                    "{:?}",
-                    inverse_transform.transform_point(follower_transform.translation())
-                );
-                // info!("{:?}", path);
+                info!("{:?}", path);
             }
         }
     }

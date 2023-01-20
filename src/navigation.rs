b8b9da7eda2bd014bd96d9b2f41ba6f826e37b5b
Jan Nils Ferner <contact@jnferner.com>
2023-01-20 20:37:51 +0100

    Try to debug path mesh
    Former-commit-id: 3558f8fdca04bc094bc7da702ec835ec89aa8227

--- src/navigation.rs
+++ src/navigation.rs
@@ -1,7 +1,9 @@
 use crate::player::{CharacterVelocity, Player};
 use crate::spawning::objects::{npc, player};
 use crate::GameState;
+use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
+use bevy_pathmesh::PathMesh;
 use bevy_pathmesh::PathmeshPlugin;
 use bevy_prototype_debug_lines::DebugLines;
 use oxidized_navigation::{
@@ -32,9 +34,7 @@ impl Plugin for NavigationPlugin {
                 max_contour_simplification_error: 0.5,
                 max_edge_length: 50,
             })
-            .add_system_set(
-                SystemSet::on_update(GameState::Playing).with_system(query_mesh), //.with_system(_draw_nav_mesh),
-            );
+            .add_system_set(SystemSet::on_update(GameState::Playing).with_system(query_mesh));
     }
 }
 
@@ -50,92 +50,30 @@ fn query_mesh(
         (With<Follower>, Without<Player>),
     >,
     with_player: Query<&GlobalTransform, (With<Player>, Without<Follower>)>,
-    nav_mesh_settings: Res<NavMeshSettings>,
-    nav_mesh: Res<NavMesh>,
     mut lines: ResMut<DebugLines>,
+    path_meshes: Res<Assets<PathMesh>>,
+    nav_meshes: Query<(&GlobalTransform, &Handle<PathMesh>), (Without<Follower>, Without<Player>)>,
 ) {
     let dt = time.delta_seconds();
-    if let Ok(nav_mesh) = nav_mesh.get().read() {
+    for (mesh_transform, path_mesh_handle) in nav_meshes.iter() {
         for (follower_transform, mut character_velocity) in &mut with_follower {
             for player_transform in &with_player {
-                let start_pos =
-                    follower_transform.translation() - Vec3::Y * npc::HEIGHT * npc::SCALE;
-                let end_pos =
-                    player_transform.translation() - Vec3::Y * player::HEIGHT * player::SCALE;
-                let end_pos = if (end_pos.y - start_pos.y).abs() > 0.1 {
-                    end_pos
-                } else {
-                    Vec3 {
-                        y: start_pos.y,
-                        ..end_pos
-                    }
+                let path_mesh = path_meshes.get(path_mesh_handle).unwrap();
+                let path = path_mesh.path(
+                    follower_transform.translation().xz(),
+                    player_transform.translation().xz(),
+                );
+                let transform = mesh_transform.compute_transform();
+                let inverse_transform = Transform {
+                    translation: -transform.translation,
+                    rotation: transform.rotation.inverse(),
+                    scale: 1. / transform.scale,
                 };
-                if (end_pos - start_pos).length_squared() < 0.75 {
-                    continue;
-                }
-
-                // Run pathfinding to get a polygon path.
-                match find_path(&nav_mesh, &nav_mesh_settings, start_pos, end_pos, Some(5.)) {
-                    Ok(path) => {
-                        // Convert polygon path to a path of Vec3s.
-                        match perform_string_pulling_on_path(&nav_mesh, start_pos, end_pos, &path) {
-                            Ok(string_path) => {
-                                let path = string_path.into_iter().chain(iter::once(end_pos));
-                                for (a, b) in path.clone().zip(path.clone().skip(1)) {
-                                    lines.line_colored(a, b, 0., Color::RED);
-                                }
-                                let next_direction = path
-                                    .map(|point| point - start_pos)
-                                    .map(|dir| {
-                                        if dir.y.abs() > 0.25 {
-                                            dir
-                                        } else {
-                                            Vec3 { y: 0., ..dir }
-                                        }
-                                    })
-                                    .filter(|dir| dir.length_squared() > 0.5)
-                                    .filter_map(|dir| dir.try_normalize())
-                                    .next();
-                                let next_direction = match next_direction {
-                                    None => {
-                                        warn!("No good direction found");
-                                        continue;
-                                    }
-                                    Some(dir) => dir,
-                                };
-                                let speed = 5.0;
-                                let velocity = next_direction * speed * dt;
-                                character_velocity.0 += velocity;
-                            }
-                            Err(error) => error!("Error with string path: {:?}", error),
-                        };
-                    }
-                    Err(_error) => {} // error!("Error with pathfinding: {:?}", _error),
-                }
-            }
-        }
-    }
-}
-
-fn _draw_nav_mesh(nav_mesh: Res<NavMesh>, mut lines: ResMut<DebugLines>) {
-    // Probably want to add in a trigger key here to make it not always draw.
-
-    if let Ok(nav_mesh) = nav_mesh.get().read() {
-        for (_, tile) in nav_mesh.get_tiles().iter() {
-            // Draw polygons.
-            for poly in tile.polygons.iter() {
-                let indices = &poly.indices;
-                for i in 0..indices.len() {
-                    let a = tile.vertices[indices[i] as usize];
-                    let b = tile.vertices[indices[(i + 1) % indices.len()] as usize];
-
-                    lines.line(a, b, 0.0);
-                }
-            }
-
-            // Draw vertex points.
-            for vertex in tile.vertices.iter() {
-                lines.line(*vertex, *vertex + Vec3::Y, 0.0);
+                info!(
+                    "{:?}",
+                    inverse_transform.transform_point(follower_transform.translation())
+                );
+                // info!("{:?}", path);
             }
         }
     }

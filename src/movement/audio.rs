daef1d341aed6572b3056b4e627e3ddc33e3fcf7
Jan Nils Ferner <contact@jnferner.com>
2023-01-28 18:06:09 +0100

    Fix annoying sound #28

--- src/movement/audio.rs
+++ src/movement/audio.rs
@@ -1,8 +1,12 @@
 use crate::file_system_interaction::asset_loading::AudioAssets;
-use crate::player_control::actions::{set_actions, Actions};
+use crate::movement::general_movement::Grounded;
+use crate::player_control::actions::set_actions;
+use crate::player_control::player_embodiment::Player;
+use crate::util::trait_extension::Vec3Ext;
 use crate::GameState;
 use bevy::prelude::*;
 use bevy_kira_audio::prelude::{Audio, *};
+use bevy_rapier3d::control::KinematicCharacterControllerOutput;
 
 pub struct InternalAudioPlugin;
 
@@ -19,36 +23,33 @@ impl Plugin for InternalAudioPlugin {
 }
 
 #[derive(Resource)]
-struct FlyingAudio(Handle<AudioInstance>);
+struct WalkingAudio(Handle<AudioInstance>);
 
 fn start_audio(mut commands: Commands, audio_assets: Res<AudioAssets>, audio: Res<Audio>) {
     audio.pause();
     let handle = audio
-        .play(audio_assets.flying.clone())
+        .play(audio_assets.walking.clone())
         .looped()
-        .with_volume(0.3)
+        .with_volume(0.8)
         .handle();
-    commands.insert_resource(FlyingAudio(handle));
+    commands.insert_resource(WalkingAudio(handle));
 }
 
 fn control_flying_sound(
-    actions: Res<Actions>,
-    audio: Res<FlyingAudio>,
+    character_query: Query<(&KinematicCharacterControllerOutput, &Grounded), With<Player>>,
+    audio: Res<WalkingAudio>,
     mut audio_instances: ResMut<Assets<AudioInstance>>,
 ) {
-    if let Some(instance) = audio_instances.get_mut(&audio.0) {
-        match instance.state() {
-            PlaybackState::Paused { .. } => {
-                if actions.player_movement.is_some() {
-                    instance.resume(AudioTween::default());
-                }
+    for (output, grounded) in character_query.iter() {
+        if let Some(instance) = audio_instances.get_mut(&audio.0) {
+            let is_in_air = grounded.time_since_last_grounded.is_active();
+            let has_horizontal_movement = !output.effective_translation.x0z().is_approx_zero();
+            let is_moving_on_ground = has_horizontal_movement && !is_in_air;
+            if is_moving_on_ground {
+                instance.resume(AudioTween::default());
+            } else {
+                instance.pause(AudioTween::default());
             }
-            PlaybackState::Playing { .. } => {
-                if actions.player_movement.is_none() {
-                    instance.pause(AudioTween::default());
-                }
-            }
-            _ => {}
         }
     }
 }

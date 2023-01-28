3c41b297df2f07c3185941549e2ad83cbd5c2755
Jan Nils Ferner <contact@jnferner.com>
2023-01-28 19:54:13 +0100

    Fix lints

--- src/level_instanciation/spawning/spawn.rs
+++ src/level_instanciation/spawning/spawn.rs
@@ -13,6 +13,7 @@ use serde::{Deserialize, Serialize};
 use std::borrow::Cow;
 use std::str::FromStr;
 
+#[allow(clippy::type_complexity)]
 pub fn spawn_requested(
     mut commands: Commands,
     gltf: Res<Assets<Gltf>>,

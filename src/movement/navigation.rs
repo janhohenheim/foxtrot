e1ccb219894031f5adaf412015f63bc1c3155108
Jan Hohenheim <jan@hohenheim.ch>
2023-02-04 03:10:23 +0100

    Fix some path finding failures
    Former-commit-id: 1baa37622f2dd29045ea0d6aefb437153a36c22f
    Former-commit-id: b42e3b564cf614ab6374928dfd71cb5bdbe2f7d8

--- src/movement/navigation.rs
+++ src/movement/navigation.rs
@@ -58,25 +58,18 @@ fn query_mesh(
                     Some((entity, _toi)) = rapier_context.cast_ray(from, to - from, max_toi, solid, filter)
                     && entity == player_entity
                 {
-                    Some(vec![to.x0z()])
+                    Some(to.x0z())
                 } else if let Some(path) = path_mesh.transformed_path(from, to) {
-                    Some(path.path)
+                    Some(path.path[0])
                 } else {
                     None
                 };
-                if let Some(path) = path && let Some(dir) = move_along_path(from, &path) {
+                if let Some(path) = path {
+                    let dir = (path - from).try_normalize().unwrap();
                     let speed = 3.;
                     character_velocity.0 += dir * dt * speed;
                 }
             }
         }
     }
-}
-
-fn move_along_path(from: Vec3, path: &[Vec3]) -> Option<Vec3> {
-    path.iter()
-        .map(|point| *point - from)
-        .filter(|dir| dir.length_squared() > 0.05)
-        .map(|dir| dir.try_normalize().unwrap_or_default())
-        .next()
 }

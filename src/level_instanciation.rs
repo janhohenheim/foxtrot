1c86fcf7fc03ffb36996b65ec17d991fb42b7000
Jan Nils Ferner <contact@jnferner.com>
2023-02-13 23:04:06 +0100

    Add docs
    Former-commit-id: 37b4345a9e0b175ada9f39e4c9b103bdefcf604b

--- src/level_instanciation.rs
+++ src/level_instanciation.rs
@@ -5,6 +5,9 @@ use crate::level_instanciation::map::MapPlugin;
 use crate::level_instanciation::spawning::SpawningPlugin;
 use bevy::prelude::*;
 
+/// Handles creation of levels and objects. Split into the following sub-plugins:
+/// - [`MapPlugin`] handles loading of level files and orchestrates the spawning of the objects therein.
+/// - [`SpawningPlugin`] handles the spawning of objects in general.
 pub struct LevelInstanciationPlugin;
 
 impl Plugin for LevelInstanciationPlugin {

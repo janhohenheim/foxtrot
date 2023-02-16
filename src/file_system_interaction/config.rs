9470ed183f5d6f2651b457f87fc2d56ace3d7029
Jan Hohenheim <jan@hohenheim.ch>
2023-02-16 15:15:04 +0100

    Parametrize entire camera
    Former-commit-id: c8159f6e283b051980564d918c7ddf5f85530de3

--- src/file_system_interaction/config.rs
+++ src/file_system_interaction/config.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy::reflect::TypeUuid;
 use serde::{Deserialize, Serialize};
+use std::f32::consts::TAU;
 
 #[derive(
     Debug, Clone, PartialEq, Reflect, FromReflect, TypeUuid, Serialize, Deserialize, Default,
@@ -11,10 +12,24 @@ pub struct GameConfig {
     pub camera: Camera,
 }
 
-#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize, Default)]
+#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize)]
 #[reflect(Serialize, Deserialize)]
 pub struct Camera {
-    fixed_angle: FixedAngle,
+    pub fixed_angle: FixedAngle,
+    pub first_person: FirstPerson,
+    pub third_person: ThirdPerson,
+    pub mouse_sensitivity: f32,
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        Self {
+            fixed_angle: FixedAngle::default(),
+            first_person: FirstPerson::default(),
+            third_person: ThirdPerson::default(),
+            mouse_sensitivity: 1e-3,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize)]
@@ -35,6 +50,56 @@ impl Default for FixedAngle {
             zoom_speed: 0.5,
             rotation_smoothing: 45.0,
             translation_smoothing: 50.0,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize)]
+#[reflect(Serialize, Deserialize)]
+pub struct FirstPerson {
+    pub translation_smoothing: f32,
+    pub rotation_smoothing: f32,
+    pub most_acute_from_above: f32,
+    pub most_acute_from_below: f32,
+}
+
+impl Default for FirstPerson {
+    fn default() -> Self {
+        Self {
+            translation_smoothing: 50.0,
+            rotation_smoothing: 45.0,
+            most_acute_from_above: TAU / 10.,
+            most_acute_from_below: TAU / 7.,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize)]
+#[reflect(Serialize, Deserialize)]
+pub struct ThirdPerson {
+    pub translation_smoothing_going_closer: f32,
+    pub translation_smoothing_going_further: f32,
+    pub rotation_smoothing: f32,
+    pub most_acute_from_above: f32,
+    pub most_acute_from_below: f32,
+    pub min_distance: f32,
+    pub max_distance: f32,
+    pub zoom_speed: f32,
+    pub min_distance_to_objects: f32,
+}
+
+impl Default for ThirdPerson {
+    fn default() -> Self {
+        Self {
+            translation_smoothing_going_closer: 100.0,
+            translation_smoothing_going_further: 50.0,
+            rotation_smoothing: 45.0,
+            most_acute_from_above: TAU / 10.,
+            most_acute_from_below: TAU / 7.,
+            min_distance: 1e-2,
+            max_distance: 10.0,
+            zoom_speed: 0.5,
+            min_distance_to_objects: 1e-2,
         }
     }
 }

f3d496163207281ed265d46b86d91910239af624
Jan Nils Ferner <contact@jnferner.com>
2023-02-20 05:05:00 +0100

    Fix missing color overrides

--- src/level_instantiation/spawning/post_spawn_modification.rs
+++ src/level_instantiation/spawning/post_spawn_modification.rs
@@ -1,5 +1,8 @@
 use crate::level_instantiation::spawning::spawn::Despawn;
+use anyhow::{Context, Result};
 use bevy::prelude::*;
+use regex::Regex;
+use std::sync::LazyLock;
 
 pub fn set_hidden(mut added_name: Query<(&Name, &mut Visibility), Added<Name>>) {
     for (name, mut visibility) in added_name.iter_mut() {
@@ -28,6 +31,7 @@ pub fn generate_tangents(
 ) {
     for event in mesh_asset_events.iter() {
         if let AssetEvent::Created { handle } = event {
+            // Guaranteed to work because we just created the mesh
             let mesh = meshes
                 .get_mut(handle)
                 .expect("Failed to get mesh even though it was just created");
@@ -36,4 +40,42 @@ pub fn generate_tangents(
             }
         }
     }
+}
+
+static COLOR_REGEX: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::new(r"\[color:\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+)\]")
+        .expect("Failed to compile color regex")
+});
+
+pub fn set_color(
+    added_name: Query<(&Name, &Children), Added<Name>>,
+    material_handles: Query<&Handle<StandardMaterial>>,
+    mut standard_materials: ResMut<Assets<StandardMaterial>>,
+) -> Result<()> {
+    for (name, children) in added_name.iter() {
+        if let Some(captures) = COLOR_REGEX.captures(&name.to_lowercase()) {
+            let color = Color::rgba_u8(
+                captures[1].parse().expect("Failed to parse color"),
+                captures[2].parse().expect("Failed to parse color"),
+                captures[3].parse().expect("Failed to parse color"),
+                captures[4].parse().expect("Failed to parse color"),
+            );
+            info!("Got color: {:?}", color);
+            let material_handle = children
+                .iter()
+                .filter_map(|entity| material_handles.get(*entity).ok())
+                .next()
+                .with_context(|| {
+                    format!(
+                        "Failed to find child containing material handle when setting color on: {}",
+                        name,
+                    )
+                })?;
+            let material = standard_materials
+                .get_mut(material_handle)
+                .context("Failed to get standard material from handle")?;
+            material.base_color = color;
+        }
+    }
+    Ok(())
 }

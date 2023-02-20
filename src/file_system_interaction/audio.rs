c0e2d01fa5a19ecb36915af842ef97531da5def2
Jan Nils Ferner <contact@jnferner.com>
2023-02-20 05:41:59 +0100

    Fix fix not actually fixing bug

--- src/file_system_interaction/audio.rs
+++ src/file_system_interaction/audio.rs
@@ -1,7 +1,5 @@
 use crate::file_system_interaction::asset_loading::AudioAssets;
-use crate::util::log_error::log_errors;
 use crate::GameState;
-use anyhow::{Context, Result};
 use bevy::prelude::*;
 use bevy_kira_audio::prelude::{Audio, *};
 
@@ -12,11 +10,7 @@ pub struct InternalAudioPlugin;
 impl Plugin for InternalAudioPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(AudioPlugin)
-            .add_system_set(SystemSet::on_exit(GameState::Loading).with_system(init_audio))
-            .add_system_set(
-                SystemSet::on_update(GameState::Playing)
-                    .with_system(pause_audio_when_time_is_paused.pipe(log_errors)),
-            );
+            .add_system_set(SystemSet::on_exit(GameState::Loading).with_system(init_audio));
     }
 }
 
@@ -33,22 +27,4 @@ fn init_audio(mut commands: Commands, audio_assets: Res<AudioAssets>, audio: Res
         .with_volume(0.8)
         .handle();
     commands.insert_resource(AudioHandles { walking: handle });
-}
-
-fn pause_audio_when_time_is_paused(
-    time: Res<Time>,
-    audio_handles: Res<AudioHandles>,
-    mut audio_instances: ResMut<Assets<AudioInstance>>,
-) -> Result<()> {
-    if !time.is_paused() {
-        return Ok(());
-    }
-    let handles = [&audio_handles.walking];
-    for handle in handles {
-        let audio_instance = audio_instances
-            .get_mut(handle)
-            .context("Failed to get audio instance from handle")?;
-        audio_instance.pause(default());
-    }
-    Ok(())
 }

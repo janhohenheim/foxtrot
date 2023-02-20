bff83a4c0f2a9a261a4b9eef3f7dd585908e5b10
Jan Nils Ferner <contact@jnferner.com>
2023-02-20 04:24:24 +0100

    Fix normal map bit depth
    Former-commit-id: ffc23461ca17e34a5bd81cca819c7b967c78d07d

--- src/level_instantiation/spawning/post_spawn_modification.rs
+++ src/level_instantiation/spawning/post_spawn_modification.rs
@@ -19,3 +19,21 @@ pub fn despawn_removed(
         }
     }
 }
+
+/// Needed for normal mapping,
+/// see [`StandardMaterial::normal_map_texture`](https://docs.rs/bevy/latest/bevy/pbr/struct.StandardMaterial.html#structfield.normal_map_texture).
+pub fn generate_tangents(
+    mut mesh_asset_events: EventReader<AssetEvent<Mesh>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    for event in mesh_asset_events.iter() {
+        if let AssetEvent::Created { handle } = event {
+            let mesh = meshes
+                .get_mut(handle)
+                .expect("Failed to get mesh even though it was just created");
+            if let Err(e) = mesh.generate_tangents() {
+                warn!("Failed to generate tangents for mesh: {}", e);
+            }
+        }
+    }
+}

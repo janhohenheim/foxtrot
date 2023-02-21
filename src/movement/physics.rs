085c25a2ed27c1b8122e8c0c8bc052ee68e866d7
Jan Nils Ferner <contact@jnferner.com>
2023-02-21 03:05:50 +0100

    Increase max dt for physics simulation
    Former-commit-id: 7072da002e3c1b6d3c9756ec0f127260d4668edb

--- src/movement/physics.rs
+++ src/movement/physics.rs
@@ -12,7 +12,14 @@ pub struct PhysicsPlugin;
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
-            .insert_resource(RapierConfiguration::default())
+            .insert_resource(RapierConfiguration {
+                timestep_mode: TimestepMode::Variable {
+                    max_dt: 1.0 / 30.0,
+                    time_scale: 1.0,
+                    substeps: 1,
+                },
+                ..default()
+            })
             .add_system_set(
                 SystemSet::on_update(GameState::Playing)
                     .with_system(read_colliders.pipe(log_errors)),

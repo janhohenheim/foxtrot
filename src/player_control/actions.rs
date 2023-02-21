90a91d24e9d0c522c25f41f746be45dff89da5c4
Jan Hohenheim <jan@hohenheim.ch>
2023-02-21 20:44:41 +0100

    Switch to LeafWingInputManager

--- src/player_control/actions.rs
+++ src/player_control/actions.rs
@@ -1,11 +1,10 @@
-use crate::player_control::actions::game_control::{get_movement, GameControl};
+use crate::player_control::actions::game_control::{get_movement, Action};
 use crate::util::trait_extension::{F32Ext, Vec2Ext};
 use crate::GameState;
 use bevy::input::mouse::{MouseMotion, MouseWheel};
 use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
 use serde::{Deserialize, Serialize};
-
-mod game_control;
 
 /// Configures [`Actions`], the resource that holds all player input.
 /// Add new input in [`set_actions`] and in [`game_control::generate_bindings!`](game_control).
@@ -39,44 +38,85 @@ impl Plugin for ActionsPlugin {
             .register_type::<ActionsFrozen>()
             .init_resource::<Actions>()
             .init_resource::<ActionsFrozen>()
+            .add_plugin(InputManagerPlugin::<PlayerAction>::default())
+            .add_plugin(InputManagerPlugin::<CameraAction>::default())
+            .add_plugin(InputManagerPlugin::<UiAction>::default())
             .add_system_set(SystemSet::on_update(GameState::Playing).with_system(set_actions));
     }
 }
 
-#[derive(
-    Debug, Clone, PartialEq, Reflect, FromReflect, Default, Resource, Serialize, Deserialize,
-)]
-#[reflect(Resource, Serialize, Deserialize)]
-pub struct Actions {
-    pub player: PlayerActions,
-    pub camera: CameraActions,
-    pub ui: UiActions,
+#[derive(Debug, Clone, Actionlike)]
+pub enum PlayerAction {
+    Move,
+    Sprint,
+    Jump,
+    Interact,
+    SpeedUpDialog,
+    NumberedChoice(u16),
 }
 
-#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Default, Serialize, Deserialize)]
-#[reflect(Serialize, Deserialize)]
-pub struct PlayerActions {
-    pub movement: Option<Vec2>,
-    pub interact: bool,
-    pub jump: bool,
-    pub sprint: bool,
+#[derive(Debug, Clone, Actionlike)]
+pub enum CameraAction {
+    Pan,
+    Zoom,
 }
 
-#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Default, Serialize, Deserialize)]
-#[reflect(Serialize, Deserialize)]
-pub struct CameraActions {
-    pub movement: Option<Vec2>,
-    pub zoom: Option<f32>,
-}
-
-#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Default, Serialize, Deserialize)]
-#[reflect(Serialize, Deserialize)]
-pub struct UiActions {
+#[derive(Debug, Clone, Actionlike)]
+pub enum UiAction {
     #[cfg(feature = "dev")]
-    pub toggle_editor: bool,
-    pub toggle_pause: bool,
-    pub numbered_choice: [bool; 10],
-    pub speed_up_dialog: bool,
+    ToggleEditor,
+    TogglePause,
+}
+
+pub fn create_player_action_input_manager_bundle() -> InputManagerBundle<PlayerAction> {
+    InputManagerBundle {
+        input_map: InputMap::new([
+            (QwertyScanCode::Space, PlayerAction::Jump),
+            (
+                VirtualDPad {
+                    up: QwertyScanCode::W,
+                    down: QwertyScanCode::S,
+                    left: QwertyScanCode::A,
+                    right: QwertyScanCode::D,
+                },
+                PlayerAction::Move,
+            ),
+            (QwertyScanCode::LShift, PlayerAction::Sprint),
+            (QwertyScanCode::E, PlayerAction::Interact),
+            (QwertyScanCode::Space, UiAction::SpeedUpDialog),
+            (QwertyScanCode::Key1, UiAction::NumberedChoice(1)),
+            (QwertyScanCode::Key2, UiAction::NumberedChoice(2)),
+            (QwertyScanCode::Key3, UiAction::NumberedChoice(3)),
+            (QwertyScanCode::Key4, UiAction::NumberedChoice(4)),
+            (QwertyScanCode::Key5, UiAction::NumberedChoice(5)),
+            (QwertyScanCode::Key6, UiAction::NumberedChoice(6)),
+            (QwertyScanCode::Key7, UiAction::NumberedChoice(7)),
+            (QwertyScanCode::Key8, UiAction::NumberedChoice(8)),
+            (QwertyScanCode::Key9, UiAction::NumberedChoice(9)),
+            (QwertyScanCode::Key0, UiAction::NumberedChoice(0)),
+        ]),
+        ..default()
+    }
+}
+
+pub fn create_camera_action_input_manager_bundle() -> InputManagerBundle<CameraAction> {
+    InputManagerBundle {
+        input_map: InputMap::default()
+            .insert(DualAxis::mouse_motion(), CameraAction::Pan)
+            .insert(DualAxis::mouse_wheel(), CameraAction::Zoom),
+        ..default()
+    }
+}
+
+pub fn create_ui_action_input_manager_bundle() -> InputManagerBundle<UiAction> {
+    InputManagerBundle {
+        input_map: InputMap::new([
+            #[cfg(feature = "dev")]
+            (QwertyScanCode::Q, UiAction::ToggleEditor),
+            (QwertyScanCode::Escape, UiAction::TogglePause),
+        ]),
+        ..default()
+    }
 }
 
 pub fn set_actions(
@@ -91,30 +131,28 @@ pub fn set_actions(
     *actions = default();
     #[cfg(feature = "dev")]
     {
-        actions.ui.toggle_editor = GameControl::ToggleEditor.just_pressed(&keyboard_input);
+        actions.ui.toggle_editor = Action::ToggleEditor.just_pressed(&keyboard_input);
     }
-    actions.ui.toggle_pause = GameControl::TogglePause.just_pressed(&keyboard_input);
-    actions.ui.speed_up_dialog = GameControl::SpeedUpDialog.pressed(&keyboard_input);
+    actions.ui.toggle_pause = Action::TogglePause.just_pressed(&keyboard_input);
+    actions.ui.speed_up_dialog = Action::SpeedUpDialog.pressed(&keyboard_input);
     for i in 0..=9 {
         actions.ui.numbered_choice[i] =
-            GameControl::NumberedChoice(i as u16).just_pressed(&keyboard_input);
+            Action::NumberedChoice(i as u16).just_pressed(&keyboard_input);
     }
     if actions_frozen.is_frozen() {
         return;
     }
 
     let player_movement = Vec2::new(
-        get_movement(GameControl::Right, &keyboard_input)
-            - get_movement(GameControl::Left, &keyboard_input),
-        get_movement(GameControl::Up, &keyboard_input)
-            - get_movement(GameControl::Down, &keyboard_input),
+        get_movement(Action::Right, &keyboard_input) - get_movement(Action::Left, &keyboard_input),
+        get_movement(Action::Up, &keyboard_input) - get_movement(Action::Down, &keyboard_input),
     );
 
     actions.player.movement =
         (!player_movement.is_approx_zero()).then(|| player_movement.normalize());
-    actions.player.jump = get_movement(GameControl::Jump, &keyboard_input) > 0.5;
-    actions.player.sprint = get_movement(GameControl::Sprint, &keyboard_input) > 0.5;
-    actions.player.interact = GameControl::Interact.just_pressed(&keyboard_input);
+    actions.player.jump = get_movement(Action::Jump, &keyboard_input) > 0.5;
+    actions.player.sprint = get_movement(Action::Sprint, &keyboard_input) > 0.5;
+    actions.player.interact = Action::Interact.just_pressed(&keyboard_input);
 
     let mut camera_movement = Vec2::ZERO;
     for event in mouse_motion.iter() {

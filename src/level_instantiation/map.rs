5c81ca4f4666465019605205a6bdacfa9e8e17bb
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-02-22 22:26:03 +0100

    Add CSS loader in WASM

--- src/level_instantiation/map.rs
+++ src/level_instantiation/map.rs
@@ -13,6 +13,8 @@ impl Plugin for MapPlugin {
             .add_system_set(
                 SystemSet::on_update(GameState::Playing).with_system(show_loading_screen),
             );
+        #[cfg(feature = "wasm")]
+        app.add_system_set(SystemSet::on_update(GameState::Playing).with_system(show_wasm_loader));
     }
 }
 
@@ -57,5 +59,40 @@ fn show_loading_screen(player_query: Query<&Player>, mut egui_context: ResMut<Eg
                 ui.label("This may take a while. Don't worry, your browser did not crash!");
             });
         });
+    }
+}
+
+#[cfg(feature = "wasm")]
+fn show_wasm_loader(player_query: Query<&Player>, mut egui_context: ResMut<EguiContext>) {
+    let id = egui::Id::new("loading-screen-shown");
+    let memory = &mut egui_context.ctx_mut().memory().data;
+    match (memory.get_temp::<()>(id), player_query.iter().next()) {
+        (None, None) => {
+            loader::show_loader();
+            memory.insert_temp(id, ());
+        }
+        (Some(_), Some(_)) => {
+            loader::hide_loader();
+            memory.remove::<()>(id);
+        }
+        _ => {}
+    }
+}
+
+#[cfg(feature = "wasm")]
+mod loader {
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen(inline_js = "
+        export function show_loader() {
+            document.querySelector('.loader').hidden = false;
+        }
+        export function hide_loader() {
+            document.querySelector('.loader').hidden = true;
+        }")]
+    extern "C" {
+        pub fn show_loader();
+
+        pub fn hide_loader();
     }
 }

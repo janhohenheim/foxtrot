9f74fa18812ca92a4ab3b6f510dc69a4cd690770
Jan Hohenheim <jan@hohenheim.ch>
2023-02-25 02:34:19 +0100

    Add warblersneeds integration

--- src/level_instantiation.rs
+++ src/level_instantiation.rs
@@ -1,6 +1,8 @@
+pub mod grass;
 pub mod map;
 pub mod spawning;
 
+use crate::level_instantiation::grass::GrassPlugin;
 use crate::level_instantiation::map::MapPlugin;
 use crate::level_instantiation::spawning::SpawningPlugin;
 use bevy::prelude::*;
@@ -8,10 +10,13 @@ use bevy::prelude::*;
 /// Handles creation of levels and objects. Split into the following sub-plugins:
 /// - [`MapPlugin`] handles loading of level files and orchestrates the spawning of the objects therein.
 /// - [`SpawningPlugin`] handles the spawning of objects in general.
+/// - [`GrassPlugin`] handles the spawning of grass on top of marked meshes.
 pub struct LevelInstantiationPlugin;
 
 impl Plugin for LevelInstantiationPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(MapPlugin).add_plugin(SpawningPlugin);
+        app.add_plugin(MapPlugin)
+            .add_plugin(SpawningPlugin)
+            .add_plugin(GrassPlugin);
     }
 }
---
+++ src/level_instantiation/grass.rs
@@ -0,0 +1,107 @@
+use crate::util::log_error::log_errors;
+use crate::util::trait_extension::MeshExt;
+use crate::GameState;
+use anyhow::{bail, Context, Result};
+use bevy::prelude::*;
+use bevy::render::mesh::{PrimitiveTopology, VertexAttributeValues};
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+use warblersneeds::prelude::*;
+
+pub struct GrassPlugin;
+
+impl Plugin for GrassPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugin(WarblersPlugin).add_system_set(
+            SystemSet::on_update(GameState::Playing).with_system(add_grass.pipe(log_errors)),
+        );
+    }
+}
+
+pub fn add_grass(
+    mut commands: Commands,
+    added_name: Query<(Entity, &Name), Added<Name>>,
+    meshes: Res<Assets<Mesh>>,
+    children_query: Query<&Children>,
+    mesh_handles: Query<&Handle<Mesh>>,
+) -> Result<()> {
+    for (parent_entity, name) in added_name.iter() {
+        if name.contains("[grass]") {
+            for (child_entity, mesh) in
+                Mesh::search_in_children(parent_entity, &children_query, &meshes, &mesh_handles)
+            {
+                let triangles = read_triangles(mesh)?;
+
+                let rng = SmallRng::from_entropy();
+                const BLADES_PER_SQUARE_METER: f32 = 10.0;
+                let blades: Vec<_> = triangles
+                    .map(|triangle| {
+                        let area = area_of_triangle(&triangle);
+                        let blade_count = (area * BLADES_PER_SQUARE_METER) as usize;
+                        let mut rng = rng.clone();
+                        (0..blade_count).into_iter().map(move |_| {
+                            let position =
+                                pick_uniform_random_point_in_triangle(&mut rng, &triangle);
+                            let height = 0.3;
+                            GrassBlade { position, height }
+                        })
+                    })
+                    .flatten()
+                    .collect();
+                let blades = (0..10000)
+                    .into_iter()
+                    .map(|i| GrassBlade {
+                        // making a grid
+                        position: Vec3::new((i / 100) as f32, 0., (i % 100) as f32),
+                        height: (i as f32).ln(),
+                    })
+                    .collect();
+
+                commands.entity(child_entity).insert(WarblersBundle {
+                    grass: Grass::new(blades),
+                    ..default()
+                });
+            }
+        }
+    }
+    Ok(())
+}
+
+fn read_triangles(mesh: &Mesh) -> Result<impl Iterator<Item = [Vec3; 3]> + '_> {
+    if mesh.primitive_topology() != PrimitiveTopology::TriangleList {
+        bail!("Grass mesh must be a triangle list");
+    }
+    let positions = mesh
+        .attribute(Mesh::ATTRIBUTE_POSITION)
+        .context("Failed to get mesh position attribute when building grass")?;
+    let positions = match positions {
+        VertexAttributeValues::Float32x3(positions) => positions,
+        _ => bail!("Grass mesh position attribute must be a float32x3"),
+    };
+    let indices = mesh
+        .indices()
+        .context("Failed to get mesh indices when building grass")?;
+    let triangle_vertex_indices = indices.iter().array_chunks::<3>();
+    let triangles = triangle_vertex_indices.map(|indices| {
+        indices.map(|index| {
+            let position = positions[index];
+            Vec3::from(position)
+        })
+    });
+    Ok(triangles)
+}
+
+/// Source: <https://math.stackexchange.com/questions/18686/uniform-random-point-in-triangle-in-3d>
+fn pick_uniform_random_point_in_triangle(rng: &mut SmallRng, &[a, b, c]: &[Vec3; 3]) -> Vec3 {
+    let r1 = rng.gen::<f32>();
+    let root_r1 = r1.sqrt();
+    let r2 = rng.gen::<f32>();
+    (1.0 - root_r1) * a + (root_r1 * (1.0 - r2)) * b + (root_r1 * r2) * c
+}
+
+/// Source: <https://math.stackexchange.com/a/128999/419398>
+fn area_of_triangle(&[a, b, c]: &[Vec3; 3]) -> f32 {
+    let ab = b - a;
+    let ac = c - a;
+    let cross = ab.cross(ac);
+    cross.length() / 2.0
+}

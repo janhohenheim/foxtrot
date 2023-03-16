a03414ecd5ebdbcf3a87d0792d5da5ad5aee0c0b
Jan Hohenheim <jan@hohenheim.ch>
2023-03-16 19:13:45 +0100

    Fix pitch clamping

--- src/player_control/camera/third_person.rs
+++ src/player_control/camera/third_person.rs
@@ -1,8 +1,8 @@
 use crate::file_system_interaction::config::GameConfig;
 use crate::player_control::actions::CameraAction;
-use crate::player_control::camera::util::clamp_pitch;
+use crate::player_control::camera::util::clamp_pitch_degrees;
 use crate::player_control::camera::{IngameCamera, IngameCameraKind};
-use crate::util::trait_extension::{F32Ext, Vec2Ext};
+use crate::util::trait_extension::Vec2Ext;
 use anyhow::{Context, Result};
 use bevy::prelude::*;
 use bevy_dolly::prelude::*;
@@ -35,54 +35,74 @@ pub fn update_rig(
             let pitch = -camera_movement.y * config.camera.mouse_sensitivity_y;
             let yaw_pitch = rig.driver_mut::<YawPitch>();
             yaw_pitch.rotate_yaw_pitch(yaw.to_degrees(), pitch.to_degrees());
-            yaw_pitch.pitch_degrees = clamp_pitch(
+            yaw_pitch.pitch_degrees = clamp_pitch_degrees(
                 camera.target.up(),
                 transform.forward(),
-                yaw_pitch.pitch_degrees.to_radians(),
+                yaw_pitch.pitch_degrees,
                 config.camera.third_person.most_acute_from_above,
                 config.camera.third_person.most_acute_from_below,
-            )
-            .to_degrees();
+            );
         }
 
-        let zoom =
-            actions.clamped_value(CameraAction::Zoom) * config.camera.third_person.zoom_speed;
+        set_desired_distance(&config, &mut camera, actions);
 
-        camera.desired_distance = (camera.desired_distance - zoom).clamp(
-            config.camera.third_person.min_distance,
-            config.camera.third_person.max_distance,
-        );
-        let current_offset = rig.driver::<Arm>().offset;
-        let origin = camera.target.translation;
-        let direction = current_offset.normalize();
-
-        let max_toi = camera.desired_distance;
-        let solid = true;
-        let mut filter = QueryFilter::only_fixed();
-        filter.flags |= QueryFilterFlags::EXCLUDE_SENSORS;
-
-        let min_distance_to_objects = config.camera.third_person.min_distance_to_objects;
-        let distance = rapier_context
-            .cast_ray(origin, direction, max_toi, solid, filter)
-            .map(|(_entity, toi)| toi - min_distance_to_objects)
-            .unwrap_or(max_toi);
-
-        let offset = direction * distance;
-
-        let original_distance_squared = current_offset.length_squared();
-        let translation_smoothing = if distance.squared() < original_distance_squared - 1e-3 {
-            config
-                .camera
-                .third_person
-                .translation_smoothing_going_closer
-        } else {
-            config
-                .camera
-                .third_person
-                .translation_smoothing_going_further
-        };
-        rig.driver_mut::<Arm>().offset = offset;
-        rig.driver_mut::<Smooth>().position_smoothness = translation_smoothing;
+        let offset = get_unbroken_line_of_sight(&rapier_context, &config, &mut camera, &rig);
+        set_arm(&config, &mut rig, offset);
     }
     Ok(())
+}
+
+fn set_desired_distance(
+    config: &GameConfig,
+    camera: &mut IngameCamera,
+    actions: &ActionState<CameraAction>,
+) {
+    let zoom = actions.clamped_value(CameraAction::Zoom) * config.camera.third_person.zoom_speed;
+
+    camera.desired_distance = (camera.desired_distance - zoom).clamp(
+        config.camera.third_person.min_distance,
+        config.camera.third_person.max_distance,
+    );
+}
+
+fn set_arm(config: &GameConfig, rig: &mut Rig, offset: Vec3) {
+    let current_offset = rig.driver::<Arm>().offset;
+    let original_distance_squared = current_offset.length_squared();
+    let translation_smoothing = if offset.length_squared() < original_distance_squared - 1e-3 {
+        config
+            .camera
+            .third_person
+            .translation_smoothing_going_closer
+    } else {
+        config
+            .camera
+            .third_person
+            .translation_smoothing_going_further
+    };
+    rig.driver_mut::<Arm>().offset = offset;
+    rig.driver_mut::<Smooth>().position_smoothness = translation_smoothing;
+}
+
+fn get_unbroken_line_of_sight(
+    rapier_context: &RapierContext,
+    config: &GameConfig,
+    camera: &IngameCamera,
+    rig: &Rig,
+) -> Vec3 {
+    let current_offset = rig.driver::<Arm>().offset;
+    let origin = camera.target.translation;
+    let direction = current_offset.normalize();
+
+    let max_toi = camera.desired_distance;
+    let solid = true;
+    let mut filter = QueryFilter::only_fixed();
+    filter.flags |= QueryFilterFlags::EXCLUDE_SENSORS;
+
+    let min_distance_to_objects = config.camera.third_person.min_distance_to_objects;
+    let distance = rapier_context
+        .cast_ray(origin, direction, max_toi, solid, filter)
+        .map(|(_entity, toi)| toi - min_distance_to_objects)
+        .unwrap_or(max_toi);
+
+    direction * distance
 }

4d928c399787602d3e6281846f7037f240692ac8
Jan Hohenheim <jan@hohenheim.ch>
2023-03-18 04:02:58 +0100

    Split off functions into module

--- src/file_system_interaction/config.rs
+++ src/file_system_interaction/config.rs
@@ -20,7 +20,7 @@ pub struct GameConfig {
     pub camera: Camera,
 }
 
-#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize, Default)]
 #[reflect(Serialize, Deserialize)]
 pub struct Camera {
     pub fixed_angle: FixedAngle,
@@ -30,19 +30,7 @@ pub struct Camera {
     pub mouse_sensitivity_y: f32,
 }
 
-impl Default for Camera {
-    fn default() -> Self {
-        Self {
-            fixed_angle: FixedAngle::default(),
-            first_person: FirstPerson::default(),
-            third_person: ThirdPerson::default(),
-            mouse_sensitivity_x: 8e-4,
-            mouse_sensitivity_y: 5e-4,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize, Default)]
 #[reflect(Serialize, Deserialize)]
 pub struct FixedAngle {
     pub min_distance: f32,
@@ -52,23 +40,10 @@ pub struct FixedAngle {
     pub translation_smoothing: f32,
     pub zoom_in_smoothing: f32,
     pub zoom_out_smoothing: f32,
+    pub pitch: f32,
 }
 
-impl Default for FixedAngle {
-    fn default() -> Self {
-        Self {
-            min_distance: 5.0,
-            max_distance: 20.0,
-            zoom_speed: 0.7,
-            rotation_smoothing: 45.0,
-            translation_smoothing: 50.0,
-            zoom_in_smoothing: 0.3,
-            zoom_out_smoothing: 1.2,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize, Default)]
 #[reflect(Serialize, Deserialize)]
 pub struct FirstPerson {
     pub translation_smoothing: f32,
@@ -78,19 +53,7 @@ pub struct FirstPerson {
     pub tracking_smoothing: f32,
 }
 
-impl Default for FirstPerson {
-    fn default() -> Self {
-        Self {
-            translation_smoothing: 50.0,
-            rotation_smoothing: 45.0,
-            max_pitch: 36.,
-            min_pitch: 50.,
-            tracking_smoothing: 0.1,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Reflect, FromReflect, Serialize, Deserialize, Default)]
 #[reflect(Serialize, Deserialize)]
 pub struct ThirdPerson {
     pub translation_smoothing: f32,
@@ -104,22 +67,4 @@ pub struct ThirdPerson {
     pub tracking_smoothing: f32,
     pub zoom_in_smoothing: f32,
     pub zoom_out_smoothing: f32,
-}
-
-impl Default for ThirdPerson {
-    fn default() -> Self {
-        Self {
-            translation_smoothing: 0.3,
-            rotation_smoothing: 40.3,
-            max_pitch: 36.,
-            min_pitch: 50.,
-            min_distance: 1e-2,
-            max_distance: 10.0,
-            zoom_speed: 0.7,
-            min_distance_to_objects: 5e-1,
-            tracking_smoothing: 0.1,
-            zoom_in_smoothing: 0.3,
-            zoom_out_smoothing: 1.2,
-        }
-    }
 }
---
+++ src/player_control/camera/rig/arm.rs
@@ -0,0 +1,107 @@
+use crate::file_system_interaction::config::GameConfig;
+use crate::player_control::camera::{IngameCamera, IngameCameraKind};
+use crate::util::smoothness_to_lerp_factor;
+use crate::util::trait_extension::F32Ext;
+use bevy::prelude::*;
+use bevy_dolly::prelude::*;
+use bevy_rapier3d::prelude::*;
+
+pub fn get_arm_distance(
+    camera: &IngameCamera,
+    transform: &Transform,
+    rapier_context: &RapierContext,
+    config: &GameConfig,
+) -> Option<f32> {
+    match camera.kind {
+        IngameCameraKind::ThirdPerson => Some(get_distance_to_collision(
+            rapier_context,
+            config,
+            camera,
+            transform,
+        )),
+        IngameCameraKind::FixedAngle => Some(camera.desired_distance),
+        _ => None,
+    }
+}
+
+pub fn get_zoom_smoothness(
+    config: &GameConfig,
+    camera: &IngameCamera,
+    rig: &Rig,
+    new_distance: f32,
+) -> f32 {
+    let current_distance = rig.driver::<Arm>().offset.z;
+    if new_distance < current_distance - 1e-4 {
+        match camera.kind {
+            IngameCameraKind::ThirdPerson => config.camera.third_person.zoom_in_smoothing,
+            IngameCameraKind::FixedAngle => config.camera.fixed_angle.zoom_in_smoothing,
+            _ => unreachable!(),
+        }
+    } else {
+        match camera.kind {
+            IngameCameraKind::ThirdPerson => config.camera.third_person.zoom_out_smoothing,
+            IngameCameraKind::FixedAngle => config.camera.fixed_angle.zoom_out_smoothing,
+            _ => unreachable!(),
+        }
+    }
+}
+
+pub fn set_arm(rig: &mut Rig, distance: f32, zoom_smoothness: f32, dt: f32) {
+    let factor = smoothness_to_lerp_factor(zoom_smoothness, dt);
+    let arm_length = &mut rig.driver_mut::<Arm>().offset.z;
+    *arm_length = arm_length.lerp(distance, factor);
+}
+
+fn get_distance_to_collision(
+    rapier_context: &RapierContext,
+    config: &GameConfig,
+    camera: &IngameCamera,
+    camera_transform: &Transform,
+) -> f32 {
+    let origin = camera.target.translation;
+    let direction = camera_transform.back();
+
+    let max_toi = camera.desired_distance;
+    let solid = true;
+    let mut filter = QueryFilter::only_fixed();
+    filter.flags |= QueryFilterFlags::EXCLUDE_SENSORS;
+
+    let min_distance = match camera.kind {
+        IngameCameraKind::ThirdPerson => config.camera.third_person.min_distance_to_objects,
+        _ => unreachable!(),
+    };
+
+    rapier_context
+        .cast_ray_and_get_normal(origin, direction, max_toi, solid, filter)
+        .map(|(_entity, ray_intersection)| {
+            get_distance_such_that_min_distance_from_collision_is_ensured(
+                ray_intersection,
+                direction,
+                min_distance,
+            )
+        })
+        .unwrap_or(max_toi)
+}
+
+fn get_distance_such_that_min_distance_from_collision_is_ensured(
+    ray_intersection: RayIntersection,
+    direction: Vec3,
+    min_distance: f32,
+) -> f32 {
+    //  Wall
+    //  ↑
+    //  │╲
+    //  │  ╲
+    //  │    ╲ Hypotenuse, direction = direction
+    //  │      ╲
+    //  │        ╲
+    //  │      ( α ╲
+    //  └─────────────→ Normal, magnitude = min_distance
+    //  │              ╲
+    //  │                ╲   Target vector. Magnitude = total length (toi) - hypotenuse
+    //  │                  ╲
+    let adjacent_side = min_distance;
+    let angle = direction.angle_between(-ray_intersection.normal);
+    let hypotenuse = adjacent_side / angle.cos();
+    ray_intersection.toi - hypotenuse
+}

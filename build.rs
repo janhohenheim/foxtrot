991a004eb6f4c9e3502c4c9b731231acfbdd609c
David <davidvanderleij@gmail.com>
2023-04-22 11:25:13 +0200

    Bump to bevy=0.10.1, bevy_asset_loader=0.15, oxidized_navigation=0.4, embed_resource=2.1

--- build.rs
+++ build.rs
@@ -5,6 +5,6 @@ fn main() {
     let target = env::var("TARGET").expect("Failed to read env var TARGET");
     if target.contains("windows") {
         // on windows we will set our game icon as icon for the executable
-        embed_resource::compile("build/windows/icon.rc");
+        embed_resource::compile("build/windows/icon.rc", embed_resource::NONE);
     }
 }

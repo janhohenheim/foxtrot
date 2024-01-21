90c4b419bf83fe99ed05d8e063cf7c3c3b344d39
Jan Hohenheim <jan@hohenheim.ch>
2024-01-21 20:48:33 +0100

    Remove warbler grass until it supports Bevy 0.12. See https://github.com/EmiOnGit/warbler_grass/issues/72
    Former-commit-id: fb9c67c20ed3a59016f33d4da2b7af695e950542

--- src/level_instantiation.rs
+++ src/level_instantiation.rs
@@ -1,8 +1,6 @@
-pub(crate) mod grass;
 pub(crate) mod map;
 pub(crate) mod spawning;
 
-use crate::level_instantiation::grass::grass_plugin;
 use crate::level_instantiation::map::map_plugin;
 use crate::level_instantiation::spawning::spawning_plugin;
 use bevy::prelude::*;
@@ -13,7 +11,5 @@ use seldom_fn_plugin::FnPluginExt;
 /// - [`spawning_plugin`] handles the spawning of objects in general.
 /// - [`grass_plugin`] handles the spawning of grass on top of marked meshes.
 pub(crate) fn level_instantiation_plugin(app: &mut App) {
-    app.fn_plugin(map_plugin)
-        .fn_plugin(spawning_plugin)
-        .fn_plugin(grass_plugin);
+    app.fn_plugin(map_plugin).fn_plugin(spawning_plugin);
 }

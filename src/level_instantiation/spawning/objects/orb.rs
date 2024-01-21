4d3303eb5b2db2714f97a6349793822fc37f12e6
Jan Hohenheim <jan@hohenheim.ch>
2024-01-22 00:02:58 +0100

    Fix lints

--- src/level_instantiation/spawning/objects/orb.rs
+++ src/level_instantiation/spawning/objects/orb.rs
@@ -4,7 +4,6 @@ use crate::shader::Materials;
 use bevy::pbr::{NotShadowCaster, NotShadowReceiver};
 use bevy::prelude::*;
 
-
 fn get_or_add_mesh_handle(mesh_assets: &mut Assets<Mesh>) -> Handle<Mesh> {
     const MESH_HANDLE: Handle<Mesh> = Handle::weak_from_u128(0x1f40128bac02a9b);
     mesh_assets.get_or_add(MESH_HANDLE, || {

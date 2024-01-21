fe7278152af99e6500b7cf4158e5e5cfadc4f89d
Jan Hohenheim <jan@hohenheim.ch>
2024-01-21 18:58:41 +0100

    Rename HandleUntyped to UntypedHandle
    Former-commit-id: d1ce1171ef232a31353c7d899cd473539330c17a

--- src/level_instantiation/spawning/objects/skydome.rs
+++ src/level_instantiation/spawning/objects/skydome.rs
@@ -12,8 +12,8 @@ use serde::{Deserialize, Serialize};
 pub(crate) struct Skydome;
 
 fn get_or_add_mesh_handle(mesh_assets: &mut Assets<Mesh>) -> Handle<Mesh> {
-    const MESH_HANDLE: HandleUntyped =
-        HandleUntyped::weak_from_u64(Mesh::TYPE_UUID, 0x1f40128bac02a9a);
+    const MESH_HANDLE: UntypedHandle =
+        UntypedHandle::weak_from_u64(Mesh::TYPE_UUID, 0x1f40128bac02a9a);
     mesh_assets.get_or_add(MESH_HANDLE, || {
         Mesh::from(shape::UVSphere {
             radius: 150.0,

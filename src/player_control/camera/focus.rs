83f77d8336d23f171fa0adbf20b7a474908f34f3
Jan Hohenheim <jan@hohenheim.ch>
2024-01-29 14:44:35 +0100

    Fix camera never targetting player
    Former-commit-id: 97332a729313ef3ea44339fcef6361de856100b9

--- src/player_control/camera/focus.rs
+++ src/player_control/camera/focus.rs
@@ -4,6 +4,7 @@ use crate::world_interaction::dialog::DialogTarget;
 use anyhow::Result;
 use bevy::prelude::*;
 use bevy_mod_sysfail::*;
+use bevy_yarn_slinger::prelude::DialogueRunner;
 use bevy_yarn_slinger_example_dialogue_view::SpeakerChangeEvent;
 
 #[sysfail(log(level = "error"))]
@@ -12,22 +13,22 @@ pub(crate) fn set_camera_focus(
     mut speaker_change_events: EventReader<SpeakerChangeEvent>,
     player_query: Query<&Transform, With<Player>>,
     dialog_targets: Query<(&Transform, &DialogTarget), Without<Player>>,
+    dialog_runner: Query<&DialogueRunner>,
 ) -> Result<()> {
-    for event in speaker_change_events.read() {
-        for mut camera in camera_query.iter_mut() {
-            for player_transform in player_query.iter() {
+    for mut camera in camera_query.iter_mut() {
+        for player_transform in player_query.iter() {
+            for event in speaker_change_events.read() {
                 if event.speaking {
                     for (dialog_target_transform, dialog_target) in dialog_targets.iter() {
                         if dialog_target.speaker == event.character_name {
                             camera.secondary_target = Some(*dialog_target_transform);
-                            camera.target = *player_transform;
                         }
                     }
-                } else {
+                } else if !dialog_runner.single().is_running() {
                     camera.secondary_target = None;
                 }
-                camera.target = *player_transform;
             }
+            camera.target = *player_transform;
         }
     }
     Ok(())

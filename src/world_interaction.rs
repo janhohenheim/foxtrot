59b9a15beaf6b6cee66d78814f225550dff7b350
Jan Hohenheim <jan@hohenheim.ch>
2024-02-01 18:37:58 +0100

    Yeet conditions
    Former-commit-id: 7524b8b1ad69a2d70b8115102e58c7a7e4290631

--- src/world_interaction.rs
+++ src/world_interaction.rs
@@ -1,19 +1,15 @@
-pub(crate) mod condition;
 pub(crate) mod dialog;
 pub(crate) mod interactions_ui;
 
-use crate::world_interaction::condition::condition_plugin;
 use crate::world_interaction::dialog::dialog_plugin;
 use crate::world_interaction::interactions_ui::interactions_ui_plugin;
 use bevy::prelude::*;
 use seldom_fn_plugin::FnPluginExt;
 
 /// Handles player to world interactions. Split in to the following sub-plugins:
-/// - [`condition_plugin`] handles trackers of player actions such as chosen dialog options
 /// - [`dialog_plugin`] handles dialog trees
 /// - [`interactions_ui_plugin`] handles the UI for interacting with an object in front of the player.
 pub(crate) fn world_interaction_plugin(app: &mut App) {
-    app.fn_plugin(condition_plugin)
-        .fn_plugin(dialog_plugin)
+    app.fn_plugin(dialog_plugin)
         .fn_plugin(interactions_ui_plugin);
 }

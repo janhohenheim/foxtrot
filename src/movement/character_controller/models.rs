86a0dd7bc97d17341f3658049e6ee204751baa01
Jan Hohenheim <jan@hohenheim.ch>
2024-02-03 22:05:24 +0100

    Fix frustum culling
    Former-commit-id: 52911ea48e1ae851c58ca9213c30b8fc6d6336dd

--- src/movement/character_controller/models.rs
+++ src/movement/character_controller/models.rs
@@ -1,23 +1,30 @@
 use crate::movement::character_controller::FloatHeight;
 use bevy::prelude::*;
+use bevy::render::view::NoFrustumCulling;
 use bevy_tnua::controller::TnuaController;
 use bevy_xpbd_3d::prelude::*;
 
 /// Shift models down because XPBD will make controllers float,
 /// but our models definitely should not be floating!
 pub(crate) fn offset_models_to_controller(
-    controllers: Query<
-        (&Transform, &FloatHeight, &Children),
-        (Added<TnuaController>, With<Collider>),
-    >,
+    mut commands: Commands,
+    controllers: Query<(Entity, &Transform, &FloatHeight), (Added<TnuaController>, With<Collider>)>,
     mut transforms: Query<&mut Transform, Without<Collider>>,
+    children_q: Query<&Children>,
 ) {
-    for (transform, float_height, children) in controllers.iter() {
+    for (entity, transform, float_height) in controllers.iter() {
         let offset = (float_height.0 / transform.scale.y) * 2.;
+        let children = children_q.get(entity).unwrap();
         for child in children.iter() {
             if let Ok(mut model_transform) = transforms.get_mut(*child) {
                 model_transform.translation.y -= offset;
             }
+        }
+
+        // Frustum culling is erroneous for animated models because the AABB is not updated.
+        commands.entity(entity).insert(NoFrustumCulling);
+        for entity in children_q.iter_descendants(entity) {
+            commands.entity(entity).insert(NoFrustumCulling);
         }
     }
 }

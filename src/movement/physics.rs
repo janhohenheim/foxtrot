88c471976b9d40cd0e3a067668a82f5718d5711f
Jan Hohenheim <jan@hohenheim.ch>
2024-02-27 13:17:41 +0100

    Fix not all child colliders having a collider
    Former-commit-id: 82c1d32a2265654c813957188a22271fc8db7b90

--- src/movement/physics.rs
+++ src/movement/physics.rs
@@ -1,6 +1,6 @@
 use crate::{level_instantiation::spawning::objects::CollisionLayer, GameState};
 use anyhow::Context;
-use bevy::prelude::*;
+use bevy::{prelude::*, transform::TransformSystem::TransformPropagate};
 use bevy_mod_sysfail::prelude::*;
 use bevy_xpbd_3d::prelude::*;
 use oxidized_navigation::NavMeshAffector;
@@ -12,7 +12,12 @@ pub(crate) fn physics_plugin(app: &mut App) {
         .add_plugins(PhysicsPlugins::default())
         // Using the default fixed timestep causes issues on faster (165 Hz) machines.
         .insert_resource(Time::new_with(Physics::variable(1.0 / 60.)))
-        .add_systems(Update, read_colliders.run_if(in_state(GameState::Playing)));
+        .add_systems(
+            Update,
+            read_colliders
+                .run_if(in_state(GameState::Playing))
+                .after(TransformPropagate),
+        );
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Component, Reflect, Serialize, Deserialize, Default)]
@@ -21,46 +26,39 @@ pub(crate) struct ColliderMarker;
 
 #[sysfail(Log<anyhow::Error, Error>)]
 pub(crate) fn read_colliders(
-    collider_marker: Query<Entity, Added<ColliderMarker>>,
+    collider_marker: Query<Entity, With<ColliderMarker>>,
     mut commands: Commands,
     children: Query<&Children>,
     meshes: Res<Assets<Mesh>>,
     mesh_handles: Query<&Handle<Mesh>>,
+    global_transforms: Query<&GlobalTransform>,
 ) {
     #[cfg(feature = "tracing")]
     let _span = info_span!("read_colliders").entered();
     for entity in collider_marker.iter() {
-        let mesh = find_mesh(entity, &children, &meshes, &mesh_handles)
-            .context("Failed to find mesh for collider")?;
-        let collider =
-            Collider::trimesh_from_mesh(mesh).context("Failed to create collider from mesh")?;
-
-        commands.entity(entity).insert((
-            collider,
-            RigidBody::Static,
-            CollisionLayers::new(
-                [CollisionLayer::Terrain, CollisionLayer::CameraObstacle],
-                [CollisionLayer::Character],
-            ),
-            NavMeshAffector,
-        ));
-    }
-}
-
-fn find_mesh<'a>(
-    parent: Entity,
-    children_query: &'a Query<&Children>,
-    meshes: &'a Assets<Mesh>,
-    mesh_handles: &'a Query<&Handle<Mesh>>,
-) -> Option<&'a Mesh> {
-    if let Ok(children) = children_query.get(parent) {
-        for child in children.iter() {
-            if let Ok(mesh_handle) = mesh_handles.get(*child) {
+        for child in children.iter_descendants(entity) {
+            if let Ok(mesh_handle) = mesh_handles.get(child) {
                 if let Some(mesh) = meshes.get(mesh_handle) {
-                    return Some(mesh);
+                    let global_transform = global_transforms
+                        .get(child)
+                        .context("Failed to get global transform while reading collider")?
+                        .compute_transform();
+                    let scaled_mesh = mesh.clone().scaled_by(global_transform.scale);
+                    let collider = Collider::trimesh_from_mesh(&scaled_mesh)
+                        .context("Failed to create collider from mesh")?;
+                    commands.entity(child).insert((
+                        collider,
+                        RigidBody::Static,
+                        CollisionLayers::new(
+                            [CollisionLayer::Terrain, CollisionLayer::CameraObstacle],
+                            [CollisionLayer::Character],
+                        ),
+                        NavMeshAffector,
+                    ));
                 }
             }
         }
+
+        commands.entity(entity).remove::<ColliderMarker>();
     }
-    None
 }

b91c8b1589671d1bf3d2c53ad751ff0c4741d4ef
Jan Hohenheim <jan@hohenheim.ch>
2024-03-05 15:40:13 +0100

    Remove stuff that is handled by the workflow
    Former-commit-id: ec780e7bb2d01acb3f7641356200dd829e753939

--- src/level_instantiation/map.rs
+++ src/level_instantiation/map.rs
@@ -16,10 +16,6 @@ pub(crate) fn map_plugin(app: &mut App) {
 
 fn spawn_level(mut commands: Commands, models: Res<Assets<Gltf>>, gltf_assets: Res<GltfAssets>) {
     let gltf = models.get(&gltf_assets.level).unwrap();
-    commands.insert_resource(AmbientLight {
-        color: Color::rgb(1., 0.65, 0.23),
-        ..default()
-    });
     commands.spawn((
         SceneBundle {
             scene: gltf.scenes[0].clone(),

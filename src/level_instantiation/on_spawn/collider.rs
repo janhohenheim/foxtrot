232bbe2106d1612eacd75f979e34963dfc5312aa
Jan Hohenheim <jan@hohenheim.ch>
2024-06-19 08:08:52 +0200

    Show camera when starting game

--- src/level_instantiation/on_spawn/collider.rs
+++ src/level_instantiation/on_spawn/collider.rs
@@ -47,8 +47,6 @@ fn spawn(
         commands
             .entity(parent)
             .remove::<Collider>()
-            // If this were on the descendant, the collider would behave as if its local transform were its global transform
-            // ¯\_(ツ)_/¯
             .insert(RigidBody::Static);
     }
 }

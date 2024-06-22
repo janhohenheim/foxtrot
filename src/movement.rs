a769c16dcdf08c8a0210db91302c0d712b260058
Jan Hohenheim <jan@hohenheim.ch>
2024-06-22 17:27:23 +0200

    Fix lints

--- src/movement.rs
+++ src/movement.rs
@@ -9,8 +9,8 @@ pub(crate) mod physics;
 /// It is further split into the following sub-plugins:
 /// - [`physics::plugin`]: Instantiates the rapier integration
 /// - [`character_controller::plugin`]: Handles kinematic character controller movement. A "character" in
-/// this sense is anything that behaves in a not-quite completely physical way, like a player, an npc, an elevator, a moving platform, etc.
-/// Contrast this with pure rigidbodies like a ball, a crate, etc.
+///     this sense is anything that behaves in a not-quite completely physical way, like a player, an npc, an elevator, a moving platform, etc.
+///     Contrast this with pure rigidbodies like a ball, a crate, etc.
 /// - [`navigation::plugin`]: Handles npc pathfinding via bevy_pathmesh integration.
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((

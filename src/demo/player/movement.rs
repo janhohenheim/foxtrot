c6be4e83a8701293ede4614e52b6b92eda0fbc59
Jan Hohenheim <jan@hohenheim.ch>
2025-04-08 00:21:31 +0200

    Add lights
    Former-commit-id: bb1d151bc031935dade816a4e556fd480d5c8a1e

--- src/demo/player/movement.rs
+++ src/demo/player/movement.rs
@@ -1,9 +1,14 @@
-use bevy::prelude::*;
+use std::f32::consts::FRAC_PI_2;
+
+use bevy::{input::mouse::AccumulatedMouseMotion, prelude::*};
 use bevy_enhanced_input::prelude::*;
 use bevy_tnua::prelude::*;
 
+use super::{Player, camera::CameraSensitivity};
+
 pub(super) fn plugin(app: &mut App) {
     app.add_observer(apply_movement).add_observer(jump);
+    app.add_systems(Update, rotate_player);
 }
 
 // All actions should implement the `InputAction` trait.
@@ -48,4 +53,39 @@ fn jump(trigger: Trigger<Fired<Jump>>, mut controllers: Query<&mut TnuaControlle
         // `TnuaBuiltinJump` also has customization fields with sensible defaults.
         ..Default::default()
     });
+}
+
+fn rotate_player(
+    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
+    mut player: Query<(&mut Transform, &CameraSensitivity), With<Player>>,
+) {
+    let Ok((mut transform, camera_sensitivity)) = player.get_single_mut() else {
+        return;
+    };
+    let delta = accumulated_mouse_motion.delta;
+
+    if delta != Vec2::ZERO {
+        // Note that we are not multiplying by delta_time here.
+        // The reason is that for mouse movement, we already get the full movement that happened since the last frame.
+        // This means that if we multiply by delta_time, we will get a smaller rotation than intended by the user.
+        // This situation is reversed when reading e.g. analog input from a gamepad however, where the same rules
+        // as for keyboard input apply. Such an input should be multiplied by delta_time to get the intended rotation
+        // independent of the framerate.
+        let delta_yaw = -delta.x * camera_sensitivity.x;
+        let delta_pitch = -delta.y * camera_sensitivity.y;
+
+        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+        let yaw = yaw + delta_yaw;
+
+        // If the pitch was ±¹⁄₂ π, the camera would look straight up or down.
+        // When the user wants to move the camera back to the horizon, which way should the camera face?
+        // The camera has no way of knowing what direction was "forward" before landing in that extreme position,
+        // so the direction picked will for all intents and purposes be arbitrary.
+        // Another issue is that for mathematical reasons, the yaw will effectively be flipped when the pitch is at the extremes.
+        // To not run into these issues, we clamp the pitch to a safe range.
+        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
+
+        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+    }
 }
--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,5 +1,6 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
+use avian3d::prelude::{PhysicsDebugPlugin, PhysicsGizmos};
 use bevy::{
     dev_tools::{
         states::log_transitions,
@@ -16,10 +17,20 @@ pub(super) fn plugin(app: &mut App) {
     app.add_systems(Update, log_transitions::<Screen>);
 
     // Toggle the debug overlay for UI.
-    app.add_plugins(DebugUiPlugin);
+    app.add_plugins((DebugUiPlugin, PhysicsDebugPlugin::default()));
+    app.insert_gizmo_config(
+        PhysicsGizmos {
+            aabb_color: Some(Color::WHITE),
+            ..default()
+        },
+        GizmoConfig {
+            enabled: false,
+            ..default()
+        },
+    );
     app.add_systems(
         Update,
-        toggle_debug_ui.run_if(input_just_pressed(TOGGLE_KEY)),
+        (toggle_debug_ui, toggle_physics_debug_ui).run_if(input_just_pressed(TOGGLE_KEY)),
     );
 }
 
@@ -27,4 +38,9 @@ const TOGGLE_KEY: KeyCode = KeyCode::Backquote;
 
 fn toggle_debug_ui(mut options: ResMut<UiDebugOptions>) {
     options.toggle();
+}
+
+fn toggle_physics_debug_ui(mut config_store: ResMut<GizmoConfigStore>) {
+    let config = config_store.config_mut::<PhysicsGizmos>().0;
+    config.enabled = !config.enabled;
 }

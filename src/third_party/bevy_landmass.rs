7533e080cda41c2311432834559f9eeea4a7a353
Jan Hohenheim <jan@hohenheim.ch>
2025-04-10 03:26:28 +0200

    Setup navmesh
    Former-commit-id: b33c6b9c7b86b7b342359be5b5f7c74705f79ebb

--- src/third_party/bevy_landmass.rs
+++ src/third_party/bevy_landmass.rs
@@ -1,12 +1,33 @@
+use crate::screens::Screen;
+
 use super::bevy_trenchbroom::Worldspawn;
+use avian3d::prelude::*;
 use bevy::prelude::*;
 use bevy_landmass::prelude::*;
-use oxidized_navigation::NavMeshAffector;
+use landmass_oxidized_navigation::{LandmassOxidizedNavigationPlugin, OxidizedArchipelago};
+use oxidized_navigation::{NavMeshAffector, NavMeshSettings, OxidizedNavigationPlugin};
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins(Landmass3dPlugin::default());
-    // TODO: add oxidized_navigation and landmass_oxidized_navigation plugins
+    app.add_plugins((
+        Landmass3dPlugin::default(),
+        LandmassOxidizedNavigationPlugin,
+        OxidizedNavigationPlugin::<Collider>::new(NavMeshSettings {
+            ..NavMeshSettings::from_agent_and_bounds(0.5, 2.0, 100.0, -20.0)
+        }),
+    ));
+    app.add_systems(OnEnter(Screen::Gameplay), setup_archipelago);
     app.add_observer(add_nav_mesh_affector_to_trenchbroom_worldspawn);
+}
+
+fn setup_archipelago(mut commands: Commands) {
+    let archipelago_entity = commands
+        .spawn((
+            Name::new("Archipelago"),
+            Archipelago3d::new(AgentOptions::default_for_agent_radius(0.5)),
+            OxidizedArchipelago,
+            StateScoped(Screen::Gameplay),
+        ))
+        .id();
 }
 
 fn add_nav_mesh_affector_to_trenchbroom_worldspawn(

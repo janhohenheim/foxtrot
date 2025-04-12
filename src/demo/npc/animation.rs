a227a1ddcafbe51a88aaa82f3091c00e8779a0f3
Jan Hohenheim <jan@hohenheim.ch>
2025-04-12 17:20:24 +0200

    Use single animation graph

--- src/demo/npc/animation.rs
+++ src/demo/npc/animation.rs
@@ -18,33 +18,49 @@ pub(super) fn plugin(app: &mut App) {
 #[derive(Debug, Component, Reflect)]
 #[reflect(Component)]
 struct NpcAnimations {
-    idle: ModelAnimation,
-    walk: ModelAnimation,
-    run: ModelAnimation,
+    idle: AnimationNodeIndex,
+    walk: AnimationNodeIndex,
+    run: AnimationNodeIndex,
 }
 
 fn setup_npc_animations(
-    trigger: Trigger<OnAdd, Npc>,
+    trigger: Trigger<OnAdd, AnimationPlayerLink>,
+    q_npc: Query<&AnimationPlayerLink, With<Npc>>,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut graphs: ResMut<Assets<AnimationGraph>>,
 ) {
-    let npc = trigger.entity();
+    let anim_player_link = trigger.entity();
+    let Ok(anim_player_link) = q_npc.get(anim_player_link) else {
+        return;
+    };
+    let anim_player = anim_player_link.0;
 
     // Create an animation graph containing a single animation. We want the "run"
     // animation from our example asset, which has an index of two.
-    let mut load_animation = |name: &str| {
-        asset_server.load_model_animation(
-            format!("{}#Animation{}", Npc::model_path(), name),
-            &mut graphs,
-        )
+    let load_animation = |name: &str| -> Handle<AnimationClip> {
+        asset_server.load(format!("{}#Animation{}", Npc::model_path(), name))
     };
+
+    let run_handle = load_animation("0");
+    let idle_handle = load_animation("1");
+    let walk_handle = load_animation("2");
+
+    let (graph, indices) = AnimationGraph::from_clips([run_handle, idle_handle, walk_handle]);
+    let [run_index, idle_index, walk_index] = indices.as_slice() else {
+        unreachable!()
+    };
+    // Store the animation graph as an asset.
+    let graph_handle = graphs.add(graph);
+
     let animations = NpcAnimations {
-        idle: load_animation("1"),
-        walk: load_animation("2"),
-        run: load_animation("0"),
+        idle: *idle_index,
+        walk: *walk_index,
+        run: *run_index,
     };
-    commands.entity(npc).insert(animations);
+    commands
+        .entity(anim_player)
+        .insert((animations, AnimationGraphHandle(graph_handle)));
 }
 
 /// Managed by [`play_animations`]
@@ -57,18 +73,19 @@ pub(crate) enum NpcAnimationState {
 }
 
 fn play_animations(
-    mut commands: Commands,
     mut query: Query<(
         &mut TnuaAnimatingState<NpcAnimationState>,
         &TnuaController,
         &AnimationPlayerLink,
-        &NpcAnimations,
     )>,
-    mut animation_players: Query<&mut AnimationPlayer>,
+    mut q_animation: Query<(&NpcAnimations, &mut AnimationPlayer)>,
 ) {
-    for (mut animating_state, controller, link, animations) in query.iter_mut() {
+    for (mut animating_state, controller, link) in query.iter_mut() {
         let animation_player_entity = link.0;
-        let mut animation_player = animation_players.get_mut(animation_player_entity).unwrap();
+        let Ok((animations, mut animation_player)) = q_animation.get_mut(animation_player_entity)
+        else {
+            continue;
+        };
         match animating_state.update_by_discriminant({
             let Some((_, basis_state)) = controller.concrete_basis::<TnuaBuiltinWalk>() else {
                 continue;
@@ -98,24 +115,15 @@ fn play_animations(
             } => match state {
                 NpcAnimationState::Airborne | NpcAnimationState::Running(..) => {
                     info!("playing run animation");
-                    animation_player.play(animations.run.index).repeat();
-                    commands
-                        .entity(animation_player_entity)
-                        .insert(AnimationGraphHandle(animations.run.graph_handle.clone()));
+                    animation_player.play(animations.run).repeat();
                 }
                 NpcAnimationState::Standing => {
                     info!("playing idle animation");
-                    animation_player.play(animations.idle.index).repeat();
-                    commands
-                        .entity(animation_player_entity)
-                        .insert(AnimationGraphHandle(animations.idle.graph_handle.clone()));
+                    animation_player.play(animations.idle).repeat();
                 }
                 NpcAnimationState::Walking(_speed) => {
                     info!("playing walk animation");
-                    animation_player.play(animations.walk.index).repeat();
-                    commands
-                        .entity(animation_player_entity)
-                        .insert(AnimationGraphHandle(animations.walk.graph_handle.clone()));
+                    animation_player.play(animations.walk).repeat();
                 }
             },
         }

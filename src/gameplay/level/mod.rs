321e59a09664d884f21386478551d26bb233f4c0
Jan Hohenheim <jan@hohenheim.ch>
2025-04-13 20:13:11 +0200

    Preload everything else
    Former-commit-id: e2f40d4e6865604c5c7a0421c875bcd79559cf0c

--- src/gameplay/level/mod.rs
+++ src/gameplay/level/mod.rs
@@ -1,5 +1,6 @@
 //! Spawn the main level.
 
+use assets::LevelAssets;
 use bevy::prelude::*;
 
 use crate::screens::Screen;
@@ -16,15 +17,10 @@ pub(super) fn plugin(app: &mut App) {
 /// Functions that accept only `&mut World` as their parameter implement [`Command`].
 /// We use this style when a command requires no configuration.
 pub(crate) fn spawn_level(world: &mut World) {
-    // The only thing we have in our level is a player,
-    // but add things like walls etc. here.
-    let asset_server = world.resource::<AssetServer>();
+    let assets = world.resource::<LevelAssets>();
     world.spawn((
         Name::new("Level"),
-        SceneRoot(
-            //  Run ./scripts/compile_maps.sh and change .map to .bsp when you're done prototyping and want some extra performance
-            asset_server.load("maps/foxtrot/foxtrot.map#Scene"),
-        ),
+        SceneRoot(assets.level.clone()),
         StateScoped(Screen::Gameplay),
         Level,
     ));
--- src/gameplay/npc/assets.rs
+++ src/gameplay/npc/assets.rs
@@ -16,13 +16,25 @@ pub(super) fn plugin(app: &mut App) {
 pub(crate) struct NpcAssets {
     #[dependency]
     pub(crate) model: Handle<Scene>,
+    #[dependency]
+    pub(crate) idle_animation: Handle<AnimationClip>,
+    #[dependency]
+    pub(crate) walk_animation: Handle<AnimationClip>,
+    #[dependency]
+    pub(crate) run_animation: Handle<AnimationClip>,
 }
 
 impl FromWorld for NpcAssets {
     fn from_world(world: &mut World) -> Self {
         let assets = world.resource::<AssetServer>();
+        let load_animation = |name: &str| -> Handle<AnimationClip> {
+            assets.load(format!("{}#Animation{}", Npc::model_path(), name))
+        };
         Self {
             model: assets.load(format!("{}#Scene0", Npc::model_path())),
+            run_animation: load_animation("0"),
+            idle_animation: load_animation("1"),
+            walk_animation: load_animation("2"),
         }
     }
 }

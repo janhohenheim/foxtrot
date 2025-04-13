085ed55afd2e5f9edf3fd98a59f29cb25159de53
Jan Hohenheim <jan@hohenheim.ch>
2025-04-13 03:48:29 +0200

    Use friendly fix for Tnua
    Former-commit-id: 8b869725b2b3330c2fe16fa64cf9e7799f606f4b

--- src/gameplay/level/props.rs
+++ src/gameplay/level/props.rs
@@ -1,13 +1,19 @@
+use std::iter;
+
 use crate::third_party::avian3d::CollisionLayer;
 use crate::third_party::bevy_trenchbroom::GetTrenchbroomModelPath as _;
 use avian3d::prelude::*;
 use bevy::{
     ecs::{component::ComponentId, world::DeferredWorld},
     prelude::*,
+    scene::SceneInstanceReady,
 };
+use bevy_tnua::TnuaNotPlatform;
 use bevy_trenchbroom::{class::QuakeClass, prelude::*};
 
-pub(super) fn plugin(_app: &mut App) {}
+pub(super) fn plugin(app: &mut App) {
+    app.add_observer(add_not_platform_to_props);
+}
 
 macro_rules! dynamic_prop {
     ($name:ident, $model:expr) => {
@@ -38,5 +44,19 @@ fn on_add_dynamic_prop<T: QuakeClass>(mut world: DeferredWorld, entity: Entity, 
             .with_default_layers(CollisionLayers::new(CollisionLayer::Prop, LayerMask::ALL)),
         RigidBody::Dynamic,
         SceneRoot(model),
+        TnuaNotPlatform,
     ));
+}
+
+fn add_not_platform_to_props(
+    trigger: Trigger<OnAdd, ColliderParent>,
+    mut commands: Commands,
+    q_collider_parent: Query<&ColliderParent>,
+    q_tnua_not_platform: Query<&TnuaNotPlatform>,
+) {
+    let parent = q_collider_parent.get(trigger.entity()).unwrap();
+    if !q_tnua_not_platform.contains(parent.get()) {
+        return;
+    }
+    commands.entity(trigger.entity()).insert(TnuaNotPlatform);
 }

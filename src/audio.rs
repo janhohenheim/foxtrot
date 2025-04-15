94e1048a3efd4a2b77630cdb67436aca6f3de4df
Jan Hohenheim <jan@hohenheim.ch>
2025-04-15 18:47:38 +0200

    Remove erring docs
    Former-commit-id: 0c1f2f9be651a9b6d82930563e1b1af6d645b9d8

--- src/audio.rs
+++ src/audio.rs
@@ -3,35 +3,13 @@ use bevy::prelude::*;
 /// An organizational marker component that should be added to a spawned [`AudioPlayer`] if it is in the
 /// general "music" category (ex: global background music, soundtrack, etc).
 ///
-/// This can then be used to query for and operate on sounds in that category. For example:
-///
-/// ```
-/// use bevy::{audio::Volume, prelude::*};
-/// use crate::audio::Music;
-///
-/// fn set_music_volume(mut sink_query: Query<&mut AudioSink, With<Music>>) {
-///     for mut sink in &mut sink_query {
-///         sink.set_volume(Volume::new(0.5));
-///     }
-/// }
-/// ```
+/// This can then be used to query for and operate on sounds in that category.
 #[derive(Component, Default)]
 pub(crate) struct Music;
 
 /// An organizational marker component that should be added to a spawned [`AudioPlayer`] if it is in the
 /// general "sound effect" category (ex: footsteps, the sound of a magic spell, a door opening).
 ///
-/// This can then be used to query for and operate on sounds in that category. For example:
-///
-/// ```
-/// use bevy::{audio::Volume, prelude::*};
-/// use crate::audio::SoundEffect;
-///
-/// fn set_sound_effect_volume(mut sink_query: Query<&mut AudioSink, With<SoundEffect>>) {
-///     for mut sink in &mut sink_query {
-///         sink.set_volume(Volume::new(0.5));
-///     }
-/// }
-/// ```
+/// This can then be used to query for and operate on sounds in that category.
 #[derive(Component, Default)]
 pub(crate) struct SoundEffect;

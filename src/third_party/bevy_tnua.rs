2bf6344bf180c3218ff06c3d46595c46210a9889
Jan Hohenheim <jan@hohenheim.ch>
2025-04-16 20:53:14 +0200

    Use newest Tnua to remove hack

--- src/third_party/bevy_tnua.rs
+++ src/third_party/bevy_tnua.rs
@@ -8,18 +8,4 @@ pub(super) fn plugin(app: &mut App) {
         TnuaControllerPlugin::new(FixedUpdate),
         TnuaAvian3dPlugin::new(FixedUpdate),
     ));
-    app.add_observer(copy_not_platform_marker_from_rigid_body_to_colliders);
-}
-
-fn copy_not_platform_marker_from_rigid_body_to_colliders(
-    trigger: Trigger<OnAdd, ColliderParent>,
-    mut commands: Commands,
-    q_collider_parent: Query<&ColliderParent>,
-    q_tnua_not_platform: Query<&TnuaNotPlatform>,
-) {
-    let parent = q_collider_parent.get(trigger.entity()).unwrap();
-    if !q_tnua_not_platform.contains(parent.get()) {
-        return;
-    }
-    commands.entity(trigger.entity()).insert(TnuaNotPlatform);
 }

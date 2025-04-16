d714e8b38bc8d88e37925122af6f77f4e09ccc2d
Jan Hohenheim <jan@hohenheim.ch>
2025-04-16 02:31:35 +0200

    Remove old stuff

--- src/gameplay/animation.rs
+++ src/gameplay/animation.rs
@@ -1,8 +1,4 @@
-//! Player sprite animation.
-//! This is based on multiple examples and may be very different for your game.
-//! - [Sprite flipping](https://github.com/bevyengine/bevy/blob/latest/examples/2d/sprite_flipping.rs)
-//! - [Sprite animation](https://github.com/bevyengine/bevy/blob/latest/examples/2d/sprite_animation.rs)
-//! - [Timers](https://github.com/bevyengine/bevy/blob/latest/examples/time/timers.rs)
+//! Animation system boilerplate.
 
 use std::iter;
 

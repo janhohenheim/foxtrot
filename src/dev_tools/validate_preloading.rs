c735b05af6bcf0f40dea73e8b706d1ef83d47ac5
Jan Hohenheim <jan@hohenheim.ch>
2025-04-16 20:53:27 +0200

    Implement asset preload validation

---
+++ src/dev_tools/validate_preloading.rs
@@ -0,0 +1,43 @@
+use bevy::prelude::*;
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_observer(validate_mesh);
+    app.add_observer(validate_material);
+    app.add_observer(validate_scene);
+}
+
+fn validate_mesh(
+    trigger: Trigger<OnAdd, Mesh3d>,
+    q_mesh: Query<&Mesh3d>,
+    assets: Res<AssetServer>,
+) {
+    let handle = &q_mesh.get(trigger.entity()).unwrap().0;
+    validate_asset(handle, &assets, "Mesh");
+}
+
+fn validate_material(
+    trigger: Trigger<OnAdd, MeshMaterial3d<StandardMaterial>>,
+    q_material: Query<&MeshMaterial3d<StandardMaterial>>,
+    assets: Res<AssetServer>,
+) {
+    let handle = &q_material.get(trigger.entity()).unwrap().0;
+    validate_asset(handle, &assets, "Material");
+}
+
+fn validate_scene(
+    trigger: Trigger<OnAdd, SceneRoot>,
+    q_scene: Query<&SceneRoot>,
+    assets: Res<AssetServer>,
+) {
+    let handle = &q_scene.get(trigger.entity()).unwrap().0;
+    validate_asset(handle, &assets, "Scene");
+}
+
+fn validate_asset<T: Asset>(handle: &Handle<T>, assets: &AssetServer, type_name: &str) {
+    let Some(path) = handle.path() else {
+        return;
+    };
+    if !assets.is_loaded_with_dependencies(handle) {
+        warn!("{type_name} at path \"{path}\" was not preloaded and will load during gameplay.",);
+    }
+}

a76e18e09e5454b8961092dd49335ff6beec4992
Jan Hohenheim <jan@hohenheim.ch>
2025-04-18 01:47:47 +0200

    Play with effects
    Former-commit-id: da9c9d01e22b2f38ac539c79436b662578f5d9c5

--- src/props/specific/burning_logs.rs
+++ src/props/specific/burning_logs.rs
@@ -27,61 +27,45 @@ pub(crate) fn setup_burning_logs(mut world: DeferredWorld, entity: Entity, _id: 
 }
 
 fn setup(effects: &mut Assets<EffectAsset>) -> Handle<EffectAsset> {
-    // Define a color gradient from red to transparent black
     let mut gradient = Gradient::new();
     gradient.add_key(0.0, Vec4::new(1., 0., 0., 1.));
     gradient.add_key(1.0, Vec4::splat(0.));
 
-    // Create a new expression module
-    let mut module = Module::default();
+    let writer = ExprWriter::new();
+    let zero = writer.lit(0.);
+    let y = writer.lit(3.).uniform(writer.lit(10.));
+    let v = zero.clone().vec3(y, zero);
+    let init_vel = SetAttributeModifier::new(Attribute::VELOCITY, v.expr());
 
-    // On spawn, randomly initialize the position of the particle
-    // to be over the surface of a sphere of radius 2 units.
+    let mut module = writer.finish();
+
     let init_pos = SetPositionSphereModifier {
         center: module.lit(Vec3::ZERO),
-        radius: module.lit(2.),
+        radius: module.lit(0.5),
         dimension: ShapeDimension::Surface,
     };
 
-    // Also initialize a radial initial velocity to 6 units/sec
-    // away from the (same) sphere center.
-    let init_vel = SetVelocitySphereModifier {
-        center: module.lit(Vec3::ZERO),
-        speed: module.lit(6.),
-    };
-
-    // Initialize the total lifetime of the particle, that is
-    // the time for which it's simulated and rendered. This modifier
-    // is almost always required, otherwise the particles won't show.
-    let lifetime = module.lit(10.); // literal value "10.0"
+    let lifetime = module.lit(10.);
     let init_lifetime = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
 
-    // Every frame, add a gravity-like acceleration downward
-    let accel = module.lit(Vec3::new(0., -3., 0.));
+    let accel = module.lit(Vec3::new(0., 1., 0.));
     let update_accel = AccelModifier::new(accel);
 
-    // Create the effect asset
-    let effect = EffectAsset::new(
-        // Maximum number of particles alive at a time
-        32768,
-        // Spawn at a rate of 5 particles per second
-        SpawnerSettings::rate(5.0.into()),
-        // Move the expression module into the asset
-        module,
-    )
-    .with_name("MyEffect")
-    .init(init_pos)
-    .init(init_vel)
-    .init(init_lifetime)
-    .update(update_accel)
-    // Render the particles with a color gradient over their
-    // lifetime. This maps the gradient key 0 to the particle spawn
-    // time, and the gradient key 1 to the particle death (10s).
-    .render(ColorOverLifetimeModifier {
-        gradient,
-        blend: ColorBlendMode::Add,
-        ..default()
-    });
+    const MAX_PARTICLES: u32 = 32768;
+    let effect = EffectAsset::new(MAX_PARTICLES, SpawnerSettings::rate(5.0.into()), module)
+        .with_name("BurningLogs")
+        .init(init_pos)
+        .init(init_vel)
+        .init(init_lifetime)
+        .update(update_accel)
+        // Render the particles with a color gradient over their
+        // lifetime. This maps the gradient key 0 to the particle spawn
+        // time, and the gradient key 1 to the particle death (10s).
+        .render(ColorOverLifetimeModifier {
+            gradient,
+            blend: ColorBlendMode::Add,
+            ..default()
+        });
 
     // Insert into the asset system
     effects.add(effect)

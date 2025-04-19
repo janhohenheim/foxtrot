b6e43d9bf0cc9f6ab77f6fad36f75286f62ebec2
Jan Hohenheim <jan@hohenheim.ch>
2025-04-20 00:59:36 +0200

    Remove dead code

--- src/props/generic.rs
+++ src/props/generic.rs
@@ -1,4 +1,4 @@
-use crate::third_party::bevy_trenchbroom::GetTrenchbroomModelPath as _;
+use crate::third_party::bevy_trenchbroom::LoadTrenchbroomModel as _;
 use crate::third_party::{avian3d::CollisionLayer, bevy_trenchbroom::fix_gltf_rotation};
 use avian3d::prelude::*;
 use bevy::{
@@ -9,23 +9,6 @@ use bevy_tnua::TnuaNotPlatform;
 use bevy_trenchbroom::{class::QuakeClass, prelude::*};
 
 pub(super) fn plugin(_app: &mut App) {}
-
-pub(crate) fn setup_dynamic_prop_with_convex_hull<T: QuakeClass>(
-    mut world: DeferredWorld,
-    entity: Entity,
-    _id: ComponentId,
-) {
-    if world.is_scene_world() {
-        return;
-    }
-
-    let bundle = dynamic_bundle::<T>(&world, ColliderConstructor::ConvexHullFromMesh);
-    world
-        .commands()
-        .entity(entity)
-        .queue(fix_gltf_rotation)
-        .insert(bundle);
-}
 
 pub(crate) fn setup_static_prop_with_convex_hull<T: QuakeClass>(
     mut world: DeferredWorld,
@@ -65,7 +48,7 @@ pub(crate) fn dynamic_bundle<T: QuakeClass>(
     world: &DeferredWorld,
     constructor: ColliderConstructor,
 ) -> impl Bundle {
-    let model = load_model::<T>(world);
+    let model = world.load_trenchbroom_model::<T>();
     (
         TransformInterpolation,
         ColliderConstructorHierarchy::new(constructor)
@@ -82,7 +65,7 @@ pub(crate) fn static_bundle<T: QuakeClass>(
     world: &DeferredWorld,
     constructor: ColliderConstructor,
 ) -> impl Bundle {
-    let model = load_model::<T>(world);
+    let model = world.load_trenchbroom_model::<T>();
     (
         ColliderConstructorHierarchy::new(constructor).with_default_layers(CollisionLayers::new(
             CollisionLayer::Default,
@@ -91,8 +74,4 @@ pub(crate) fn static_bundle<T: QuakeClass>(
         RigidBody::Static,
         SceneRoot(model),
     )
-}
-
-fn load_model<T: QuakeClass>(world: &DeferredWorld) -> Handle<Scene> {
-    world.resource::<AssetServer>().load(T::scene_path())
 }

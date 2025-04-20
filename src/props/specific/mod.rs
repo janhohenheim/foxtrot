c75d32557b44c750d94197886df4ec8cc619e3b1
Jan Hohenheim <jan@hohenheim.ch>
2025-04-20 17:47:27 +0200

    Add more docs

--- src/props/specific/mod.rs
+++ src/props/specific/mod.rs
@@ -28,18 +28,21 @@ pub(crate) fn setup_chair(mut world: DeferredWorld, entity: Entity, _id: Compone
     if world.is_scene_world() {
         return;
     }
-    let bundle = dynamic_bundle::<Chair>(&world, ColliderConstructor::ConvexDecompositionFromMesh);
+    let model = world.load_trenchbroom_model::<Chair>();
     world
         .commands()
         .entity(entity)
         .queue(fix_gltf_rotation)
-        .insert(bundle)
-        .insert(
+        .insert((
             ColliderConstructorHierarchy::new(ColliderConstructor::ConvexDecompositionFromMesh)
                 .with_default_layers(CollisionLayers::new(CollisionLayer::Prop, LayerMask::ALL))
                 // Make the chair way more dense than the default, as it feels janky to be able to push it around easily.
                 .with_default_density(10_000.0),
-        );
+            TransformInterpolation,
+            RigidBody::Dynamic,
+            // Not inserting `TnuaNotPlatform` so that the player can jump on it.
+            SceneRoot(model),
+        ));
 }
 
 pub(crate) fn setup_crate(mut world: DeferredWorld, entity: Entity, _id: ComponentId) {

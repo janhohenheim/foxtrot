7f2c116586f75a3bf176ba723262d18b919f878c
Jan Hohenheim <jan@hohenheim.ch>
2025-04-24 20:45:21 +0200

    Change asset preloading strategy
    Former-commit-id: b34c3cbcf08b1b05f2f40978328aba885ffebb27

--- src/gameplay/crosshair/assets.rs
+++ src/gameplay/crosshair/assets.rs
@@ -4,25 +4,28 @@ use bevy::prelude::*;
 
 use crate::asset_tracking::LoadResource;
 
+pub(crate) const CROSSHAIR_DOT_PATH: &str = "ui/crosshair_dot.png";
+pub(crate) const CROSSHAIR_SQUARE_PATH: &str = "ui/crosshair_square.png";
+
 pub(super) fn plugin(app: &mut App) {
-    app.register_type::<CursorAssets>();
     app.load_resource::<CursorAssets>();
 }
 
 #[derive(Resource, Asset, Clone, Reflect)]
 #[reflect(Resource)]
-pub(crate) struct CursorAssets {
+pub struct CursorAssets {
     #[dependency]
-    pub(crate) crosshair_dot: Handle<Image>,
-    pub(crate) crosshair_square: Handle<Image>,
+    crosshair_dot: Handle<Image>,
+    #[dependency]
+    crosshair_square: Handle<Image>,
 }
 
 impl FromWorld for CursorAssets {
     fn from_world(world: &mut World) -> Self {
         let assets = world.resource::<AssetServer>();
         Self {
-            crosshair_dot: assets.load("ui/crosshair_dot.png"),
-            crosshair_square: assets.load("ui/crosshair_square.png"),
+            crosshair_dot: assets.load(CROSSHAIR_DOT_PATH),
+            crosshair_square: assets.load(CROSSHAIR_SQUARE_PATH),
         }
     }
 }

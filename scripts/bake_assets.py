3fedca2393b73ade09f15d865ab876551b4e13cb
Jan Hohenheim <jan@hohenheim.ch>
2025-05-03 00:31:11 +0200

    Work on asset bake file

--- scripts/bake_assets.py
+++ scripts/bake_assets.py
@@ -2,11 +2,16 @@
 
 import subprocess
 import sys
+import os
+
+ORIGINAL_ASSETS_DIR = "assets"
+BAKED_ASSETS_DIR = "assets_baked"
 
 def main():
-    verify_that_tools_are_installed()
+    verify_that_the_assets_are_in_the_working_directory()
+    verify_that_all_tools_are_installed()
 
-def verify_that_tools_are_installed():
+def verify_that_all_tools_are_installed():
     tools = [["kram"], ["qbsp", "--help"], ["light", "--help"]]
     for tool in tools:
         try:
@@ -14,6 +19,11 @@ def verify_that_tools_are_installed():
         except FileNotFoundError:
             print(f"{tool[0]} is not installed")
             sys.exit(1)
+
+def verify_that_the_assets_are_in_the_working_directory():
+    if not os.path.exists(ORIGINAL_ASSETS_DIR):
+        print(f"'{ORIGINAL_ASSETS_DIR}' directory not found. Please run this script from the root of the repository.")
+        sys.exit(1)
 
 
 

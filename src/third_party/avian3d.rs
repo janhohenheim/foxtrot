98f4f12bae9d3acb45681d987347bf095a17757a
Jan Hohenheim <jan@hohenheim.ch>
2025-05-03 17:49:06 +0200

    Remove manual interpolation opt-in

--- src/third_party/avian3d.rs
+++ src/third_party/avian3d.rs
@@ -5,6 +5,7 @@ use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(PhysicsPlugins::default());
+    app.add_observer(enable_interpolation);
 }
 
 #[derive(Debug, PhysicsLayer, Default)]
@@ -13,4 +14,19 @@ pub(crate) enum CollisionLayer {
     Default,
     Prop,
     Character,
+}
+
+fn enable_interpolation(
+    trigger: Trigger<OnAdd, RigidBody>,
+    rigid_body: Query<&RigidBody>,
+    mut commands: Commands,
+) {
+    let Ok(rigid_body) = rigid_body.get(trigger.target()) else {
+        return;
+    };
+    if rigid_body.is_dynamic() {
+        commands
+            .entity(trigger.target())
+            .insert(TransformInterpolation);
+    }
 }

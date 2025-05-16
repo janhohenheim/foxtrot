5088b770227d7bf6c2e79f8e3d14dfb475ac06bc
Jan Hohenheim <jan@hohenheim.ch>
2025-05-16 19:01:42 +0200

    Bump BTB

--- src/props/brush_entity/light_window.rs
+++ src/props/brush_entity/light_window.rs
@@ -12,40 +12,20 @@ pub(super) fn plugin(app: &mut App) {
 #[reflect(QuakeClass, Component)]
 #[base(Transform, Visibility)]
 #[spawn_hooks(SpawnHooks::new().convex_collider().smooth_by_default_angle())]
-pub(crate) struct LightWindow {
-    angles: Vec3,
-}
+pub(crate) struct LightWindow;
 
-fn setup_light_window_brush_entity(
-    trigger: Trigger<OnAdd, LightWindow>,
-    brush_entity: Query<(&LightWindow, &Children)>,
-    mut commands: Commands,
-) {
+fn setup_light_window_brush_entity(trigger: Trigger<OnAdd, LightWindow>, mut commands: Commands) {
     let entity = trigger.target();
-    let Ok((light_window, children)) = brush_entity.get(entity) else {
-        return;
-    };
-    let rotation = Quat::from_euler(
-        EulerRot::XYZ,
-        light_window.angles.x.to_radians(),
-        light_window.angles.y.to_radians(),
-        light_window.angles.z.to_radians(),
-    );
-    for brush in children.iter() {
-        commands
-            .entity(brush)
-            .insert(children![(
-                SpotLight {
-                    color: Color::srgb_u8(239, 173, 144),
-                    intensity: 200_000.0,
-                    radius: 0.1,
-                    shadows_enabled: true,
-                    #[cfg(feature = "native")]
-                    soft_shadows_enabled: true,
-                    ..default()
-                },
-                Transform::IDENTITY.with_rotation(rotation),
-            )])
-            .queue(disable_shadow_casting);
-    }
+    commands
+        .entity(entity)
+        .with_child(SpotLight {
+            color: Color::srgb_u8(239, 173, 144),
+            intensity: 200_000.0,
+            radius: 0.1,
+            shadows_enabled: true,
+            #[cfg(feature = "native")]
+            soft_shadows_enabled: true,
+            ..default()
+        })
+        .queue(disable_shadow_casting);
 }

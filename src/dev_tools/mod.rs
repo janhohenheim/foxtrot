bc580070004b97e99234e113fe3ae41a06288100
Jan Hohenheim <jan@hohenheim.ch>
2025-05-20 20:45:06 +0200

    Add hotpatching

--- src/dev_tools/mod.rs
+++ src/dev_tools/mod.rs
@@ -1,6 +1,7 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
 use bevy::{dev_tools::states::log_transitions, prelude::*};
+use bevy_simple_subsecond_system::prelude::*;
 
 mod debug_ui;
 mod input;
@@ -15,5 +16,17 @@ pub(super) fn plugin(app: &mut App) {
         (log_transitions::<Screen>, log_transitions::<LoadingScreen>).chain(),
     );
 
-    app.add_plugins((debug_ui::plugin, input::plugin, validate_preloading::plugin));
+    app.add_plugins((
+        SimpleSubsecondPlugin::default(),
+        debug_ui::plugin,
+        input::plugin,
+        validate_preloading::plugin,
+    ));
+
+    app.add_systems(Update, debug_print);
+}
+
+#[hot]
+fn debug_print() {
+    println!("Debug");
 }

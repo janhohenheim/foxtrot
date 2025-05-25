7e68988fae3c79f0d5921f1fcc543ad17fe495a8
Jan Hohenheim <jan@hohenheim.ch>
2025-05-25 16:55:30 +0200

    Restrict expensive navmesh sampling to every 500 ms

--- src/gameplay/player/navmesh_position.rs
+++ src/gameplay/player/navmesh_position.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use bevy::prelude::*;
 use bevy_landmass::{Archipelago3d, FromAgentRadius, PointSampleDistance3d};
 #[cfg(feature = "hot_patch")]
@@ -23,7 +25,15 @@ pub(crate) struct LastValidPlayerNavmeshPosition(pub(crate) Option<Vec3>);
 fn update_last_valid_player_navmesh_position(
     player_character: Single<(&GlobalTransform, &mut LastValidPlayerNavmeshPosition)>,
     archipelago: Single<&Archipelago3d>,
+    time: Res<Time>,
+    mut timer: Local<Option<Timer>>,
 ) {
+    let timer = timer.get_or_insert(Timer::new(Duration::from_millis(500), TimerMode::Once));
+    timer.tick(time.delta());
+    if !timer.finished() {
+        return;
+    }
+    timer.reset();
     let (transform, mut last_valid_player_navmesh_position) = player_character.into_inner();
     let sampled_point = archipelago.sample_point(
         transform.translation(),
